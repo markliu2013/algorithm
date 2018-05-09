1c31ab98dbe6a154b2f72543ed470d782a25eb6d
markliu2013 <markliu2013@gmail.com>
2018-05-09 21:49:45 +0800

    MinAvgTwoSlice

--- codility/src/main/java/com/zfwhub/algorithm/codility/prefix/sums/MinAvgTwoSlice.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/prefix/sums/MinAvgTwoSlice.java
@@ -3,7 +3,22 @@ package com.zfwhub.algorithm.codility.prefix.sums;
 public class MinAvgTwoSlice {
 
     public int solution(int[] A) {
-        return 0;
+        float minAvg = Float.MAX_VALUE;
+        int minAvgIndex = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = i+1; j < A.length; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += A[k];
+                }
+                float avg = sum / (float) (j-i+1);
+                if (minAvg > avg) {
+                    minAvg = avg;
+                    minAvgIndex = i;
+                }
+            }
+        }
+        return minAvgIndex;
     }
 
 }
--- codility/src/test/java/com/zfwhub/algorithm/codility/prefix/sums/MinAvgTwoSliceTest.java
+++ codility/src/test/java/com/zfwhub/algorithm/codility/prefix/sums/MinAvgTwoSliceTest.java
@@ -2,15 +2,33 @@ package com.zfwhub.algorithm.codility.prefix.sums;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
 
 public class MinAvgTwoSliceTest {
 
+    private static MinAvgTwoSlice minAvgTwoSlice;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        minAvgTwoSlice = new MinAvgTwoSlice();
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
 
     @Test
     public void testSolution() {
-
-        fail("Not yet implemented");
+        int result = minAvgTwoSlice.solution(new int[] {4,2,2,5,1,5,8});
+        assertEquals(1, result);
     }
 
 }
