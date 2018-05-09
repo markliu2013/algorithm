054a1b8988be98f9567d150d59b8861540a31bca
markliu2013 <markliu2013@gmail.com>
2018-05-09 21:14:20 +0800

    MinAvgTwoSlice

---
+++ codility/src/main/java/com/zfwhub/algorithm/codility/prefix/sums/MinAvgTwoSlice.java
@@ -0,0 +1,9 @@
+package com.zfwhub.algorithm.codility.prefix.sums;
+
+public class MinAvgTwoSlice {
+
+    public int solution(int[] A) {
+        return 0;
+    }
+
+}
---
+++ codility/src/test/java/com/zfwhub/algorithm/codility/prefix/sums/MinAvgTwoSliceTest.java
@@ -0,0 +1,16 @@
+package com.zfwhub.algorithm.codility.prefix.sums;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MinAvgTwoSliceTest {
+
+
+    @Test
+    public void testSolution() {
+
+        fail("Not yet implemented");
+    }
+
+}
