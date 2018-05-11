91b5d8aa915b9e5cf44e457df500860124ef3337
markliu2013 <markliu2013@gmail.com>
2018-05-11 13:17:18 +0800

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/markliu2013/algorithm
    
    # Conflicts:
    #	codility/src/main/java/com/zfwhub/algorithm/codility/counting/elements/MaxCounters.java
    #	codility/src/main/java/com/zfwhub/algorithm/codility/counting/elements/MissingInteger.java

--- codility/src/main/java/com/zfwhub/algorithm/codility/arrays/CyclicRotation.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/arrays/CyclicRotation.java
@@ -1,8 +1,9 @@
 package com.zfwhub.algorithm.codility.arrays;
 
 import java.util.Arrays;
+
 /**
- *
+ * 将数组位移指定的位数
  * https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/start/
  * https://mark.zfwhub.com/java-solution-to-cyclicrotation.html
  */
@@ -13,10 +14,10 @@ public class CyclicRotation {
      */
     public int[] solution(int[] A, int K) {
         int times = A.length == 0 ? 0 : K % A.length;
-        for (int i=0; i<times; i++) {
+        for (int i = 0; i < times; i++) {
             int temp = A[0];
-            int last = A[A.length-1];
-            for (int j=1; j<A.length; j++) {
+            int last = A[A.length - 1];
+            for (int j = 1; j < A.length; j++) {
                 int swap = A[j];
                 A[j] = temp;
                 temp = swap;
@@ -29,28 +30,28 @@ public class CyclicRotation {
     public int[] solution2(int[] A, int K) {
         int times = A.length == 0 ? 0 : K % A.length;
         int[] B = new int[A.length];
-        for (int i=times; i<A.length; i++) {
-            B[i] = A[i-times];
+        for (int i = times; i < A.length; i++) {
+            B[i] = A[i - times];
         }
-        for (int i=0; i<times; i++) {
-            B[i] = A[A.length-times+i];
+        for (int i = 0; i < times; i++) {
+            B[i] = A[A.length - times + i];
         }
         return B;
     }
 
     public int[] solution3(int[] A, int K) {
         int times = A.length == 0 ? 0 : K % A.length;
-        //int[] B = Arrays.copyOfRange(A, A.length-times, A.length);
-        //int[] C = Arrays.copyOfRange(A, 0, A.length-times);
+        // int[] B = Arrays.copyOfRange(A, A.length-times, A.length);
+        // int[] C = Arrays.copyOfRange(A, 0, A.length-times);
         int[] D = new int[A.length];
-        System.arraycopy(A, 0, D, times, A.length-times);
-        System.arraycopy(A, A.length-times, D, 0, times);
+        System.arraycopy(A, 0, D, times, A.length - times);
+        System.arraycopy(A, A.length - times, D, 0, times);
         return D;
     }
 
     public static void main(String[] args) {
-        CyclicRotation cyclicRotation=new CyclicRotation();
-        int[] B=cyclicRotation.solution3(new int[]{3, 8, 9, 7, 6}, 1);
+        CyclicRotation cyclicRotation = new CyclicRotation();
+        int[] B = cyclicRotation.solution3(new int[] { 3, 8, 9, 7, 6 }, 1);
         System.out.println(Arrays.toString(B));
     }
 }
