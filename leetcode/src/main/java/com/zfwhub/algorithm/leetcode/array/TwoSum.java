6209209e5f501596aba3871ee7489d0756b14cdc
markliu2013 <markliu2013@gmail.com>
2018-05-11 20:58:38 +0800

    LeetCode TwoSum

---
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/array/TwoSum.java
@@ -0,0 +1,64 @@
+package com.zfwhub.algorithm.leetcode.array;
+
+import java.util.HashMap;
+import java.util.Arrays;
+
+/**
+ * 在一个数组中找两个数的和等于指定的数
+ * https://leetcode.com/problems/two-sum/solution/
+ */
+public class TwoSum {
+
+    /**
+     * straightforward
+     */
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * 使用HashMap优化
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * HashMap一次for循环
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+        int[] result = twoSum.twoSum(new int[] { 2, 7, 11, 15 }, 13);
+        System.out.println(Arrays.toString(result));
+    }
+}
