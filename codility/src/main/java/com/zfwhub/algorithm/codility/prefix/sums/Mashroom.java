26f26c67c2acc5d0e59eed4b650fc59a8bb07a0e
markliu2013 <markliu2013@gmail.com>
2018-05-11 18:43:44 +0800

    prefixSum

---
+++ codility/src/main/java/com/zfwhub/algorithm/codility/prefix/sums/Mashroom.java
@@ -0,0 +1,16 @@
+package com.zfwhub.algorithm.codility.prefix.sums;
+
+/**
+ * https://codility.com/media/train/3-PrefixSums.pdf
+ */
+public class Mashroom {
+    // TODO:
+    public int maxCollect(int[] arr, int k, int m) {
+        return 0;
+    }
+    public static void main(String[] args) {
+        Mashroom m = new Mashroom();
+        int[] arr = new int[] {2, 3, 7, 5, 1, 3, 9};
+        System.out.println(m.maxCollect(arr, 4, 6));
+    }
+}
---
+++ codility/src/main/java/com/zfwhub/algorithm/codility/prefix/sums/PassingCars.java
@@ -0,0 +1,63 @@
+package com.zfwhub.algorithm.codility.prefix.sums;
+
+/**
+ * https://app.codility.com/programmers/lessons/5-prefix_sums/passing_cars/
+ */
+public class PassingCars {
+    /**
+     * count每个0前面有几个1
+     */
+    public int solution(int[] A) {
+        int count = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[i] == 0 && A[j] == 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 利用prefixSum, 还是计算每个0前面几个1 
+     */
+    public int solution2(int[] A) {
+        int[] prefixSum = new int[A.length];
+        prefixSum[0] = A[0];
+        for (int i = 1; i < A.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + A[i];
+        }
+        int count = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 0) {
+                count += prefixSum[A.length - 1] - prefixSum[i];
+                if (count > 1000000000) {
+                    return -1;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 计算每个1前面几个0
+     */
+    public int solution3(int[] A) {
+        int countOfZeros = 0, count = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 0)
+                countOfZeros++;
+            if (A[i] == 1)
+                count += countOfZeros;
+            if (count > 1000000000)
+                return -1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        PassingCars pc = new PassingCars();
+        System.out.println(pc.solution2(new int[] { 0, 1, 0, 1, 1 }));
+    }
+}
