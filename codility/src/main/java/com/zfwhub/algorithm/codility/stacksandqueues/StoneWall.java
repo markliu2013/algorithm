c643763dffef40c8d351dde02f7a6177c07b0d87
markliu2013 <markliu2013@gmail.com>
2018-05-15 22:20:33 +0800

    StoneWall

--- codility/src/main/java/com/zfwhub/algorithm/codility/stacksandqueues/StoneWall.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/stacksandqueues/StoneWall.java
@@ -1,14 +1,32 @@
 package com.zfwhub.algorithm.codility.stacksandqueues;
+
+import java.util.HashSet;
+
 /**
  * https://app.codility.com/programmers/lessons/7-stacks_and_queues/stone_wall/
  */
 public class StoneWall {
     // TODO
-    public int solution(int[] H) {
-        return 0;
+    public static int solution(int[] H) {
+        int count = 0;
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < H.length; i++) {
+            if (set.contains(H[i])) {
+                for (int j = 0; j < H[i]; j++) {
+                    set.remove(j);
+                }
+            } else {
+                set.add(H[i]);
+                count++;
+                for (int j = 0; j < H[i]; j++) {
+                    set.remove(j);
+                }
+            }
+        }
+        return count;
     }
     
     public static void main(String[] args) {
-        
+        System.out.println(StoneWall.solution(new int[] {8,8,5,7,9,8,7,4,8}));
     }
 }
