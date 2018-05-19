f5a507571aba638e77ce8b18409e5027e9c30c47
markliu2013 <markliu2013@gmail.com>
2018-05-19 14:38:48 +0800

    MaxProfit

--- codility/src/main/java/com/zfwhub/algorithm/codility/maximum_slice_problem/MaxProfit.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/maximum_slice_problem/MaxProfit.java
@@ -1,5 +1,22 @@
 package com.zfwhub.algorithm.codility.maximum_slice_problem;
 
 public class MaxProfit {
-
+    
+    /**
+     * straightforward, brute force 
+     */
+    public static int solution(int[] A) {
+        int maxProfit = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = i+1; j < A.length; j++) {
+                maxProfit = Math.max(maxProfit, A[j] - A[i]);
+            }
+        }
+        return maxProfit;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(MaxProfit.solution(new int[] {23171,21011,21123,21366,21013,21367}));
+    }
+    
 }
