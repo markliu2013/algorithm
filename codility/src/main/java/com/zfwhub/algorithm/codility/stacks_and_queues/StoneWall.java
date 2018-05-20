74aaf315d6d958712fbb095326f50bc41b4e1f35
markliu2013 <markliu2013@gmail.com>
2018-05-20 21:59:52 +0800

    StoneWall

--- codility/src/main/java/com/zfwhub/algorithm/codility/stacks_and_queues/StoneWall.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/stacks_and_queues/StoneWall.java
@@ -1,32 +1,50 @@
 package com.zfwhub.algorithm.codility.stacks_and_queues;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 /**
  * https://app.codility.com/programmers/lessons/7-stacks_and_queues/stone_wall/
  */
 public class StoneWall {
-    // TODO
+    
+    /**
+     * brute force
+     */
     public static int solution(int[] H) {
         int count = 0;
         HashSet<Integer> set = new HashSet<Integer>();
         for (int i = 0; i < H.length; i++) {
             if (set.contains(H[i])) {
-                for (int j = 0; j > H[i]; j++) {
-                    set.remove(j);
+                Iterator<Integer> iterator = set.iterator();
+                while (iterator.hasNext()) {
+                    Integer element = iterator.next();
+                    if (element >  H[i]) {
+                        iterator.remove();
+                    }
                 }
             } else {
                 set.add(H[i]);
                 count++;
-                for (int j = 0; j < H[i]; j++) {
-                    set.remove(j);
+                Iterator<Integer> iterator = set.iterator();
+                while (iterator.hasNext()) {
+                    Integer element = iterator.next();
+                    if (element > H[i]) {
+                        iterator.remove();
+                    }
                 }
             }
         }
         return count;
     }
     
+    // TODO StoneWall
+    public static int solution2(int[] H) {
+        return 0;
+    }
+    
     public static void main(String[] args) {
         System.out.println(StoneWall.solution(new int[] {8,8,5,7,9,8,7,4,8}));
+//        System.out.println(StoneWall.solution(new int[] {1,2,3,1,2}));
     }
 }
