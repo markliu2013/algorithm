a3a97b09de846f70e5cd5fd9e3ea229ef93f6ee1
markliu2013 <markliu2013@gmail.com>
2018-05-21 22:45:11 +0800

    MaxSliceSum

--- codility/src/main/java/com/zfwhub/algorithm/codility/maximum_slice_problem/MaxSliceSum.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/maximum_slice_problem/MaxSliceSum.java
@@ -1,5 +1,6 @@
 package com.zfwhub.algorithm.codility.maximum_slice_problem;
 
+import java.util.Arrays;
 
 /**
  * https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_slice_sum/
@@ -32,10 +33,10 @@ public class MaxSliceSum {
     
     // TODO sliding window, maybe.
     /**
-     * Ö»Òª³öÏÖÒ»¸öÕýÊý£¬¶øÇÒÖ®Ç°µÄ×ÜºÍÎª¸ºÊý£¬ÔòÖ®Ç°µÄ¶¼ÌÔÌ­
+     * 
      *  
      */
-    public static int solution2(int[] A) {
+    public static int solution3(int[] A) {
         int[] prefixSums = new int[A.length];
         prefixSums[0] = A[0];
         for (int i = 1; i < A.length; i++) {
@@ -47,27 +48,84 @@ public class MaxSliceSum {
         int endIndex = 0;//max end index
         int currentStartIndex = 0;
         for (int i = 1; i < A.length; i++) {
-            if (A[i] < 0) {
+            if (A[i] <= 0) {
                 
             } else {
-                
-            }
-            
-            if (prefixSums[i] - prefixSums[endIndex] > 0) {
-                endIndex = i;
-                maxSliceSum += prefixSums[i] - prefixSums[endIndex];
-            } else {
-                if (A[i] > maxSliceSum) {
-                    
+                if (prefixSums[i-1] < 0) {//é‡åˆ°æ­£æ•°ï¼Œå‰é¢çš„åŠ èµ·æ¥å°äºŽ0
+                    if (A[i] > maxSliceSum) {//è¿™ä¸ªæ•°å¾ˆå¤§ï¼Œå‰é¢çš„éƒ½æ·˜æ±°ã€‚
+                        maxSliceSum = A[i];
+                        startIndex = i;
+                        endIndex = i;
+                        currentStartIndex = i;
+                    } else {//ç»§ç»­è§‚å¯Ÿ
+                        currentStartIndex = i;
+                    }
+                }
+                if (prefixSums[i] - prefixSums[endIndex] > 0) {
+                    endIndex = i;
+                    maxSliceSum += prefixSums[i] - prefixSums[endIndex];
                 }
             }
             
         }
         return maxSliceSum;
     }
+    public static int solution2(int[] A) {
+        int[] prefixSums = new int[A.length];
+        prefixSums[0] = A[0];
+        for (int i = 1; i < A.length; i++) {
+            prefixSums[i] = prefixSums[i-1] + A[i];
+        }
+        int maxSliceSum = A[0];
+        int startIndex = 0;// max start index
+        int endIndex = 0;//max end index
+        int currentStartIndex = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (prefixSums[i] - prefixSums[endIndex] > 0) {
+                maxSliceSum += prefixSums[i] - prefixSums[endIndex];
+                endIndex = i;
+            }
+            
+        }
+        return maxSliceSum;
+    }
+    
+    public static int solution4(int[] a) {
+        // growing negative -4,-3,-2,-1
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < a.length; i++) {
+            max = Math.max(max, a[i]);
+        }
+        if (max <= 0) {
+            return max;
+        }
+        
+        int start=0,end=0,curr_max=0,prev_max=0,start_o=0,i;
+        prev_max = a[0];
+        int n = a.length;
+        for(i=0; i<n; i++){
+            curr_max += a[i];
+            if(curr_max < 0){
+                start = i+1;
+                curr_max = 0;
+            }
+            else if(curr_max > prev_max){
+                end = i;
+                start_o = start;
+                prev_max = curr_max;
+            }
+
+        }
+        System.out.println(start_o + " " + end);
+        int sum = 0;
+        for (int j = start_o; j <= end; j++) {
+            sum += a[j];
+        }
+        return sum;
+    }
     
     public static void main(String[] args) {
-        System.out.println(MaxSliceSum.solution2(new int[] {3,2,-6,4,0}));
+        System.out.println(MaxSliceSum.solution4(new int[] {-4,-3,-2,-1}));
     }
 
 }
