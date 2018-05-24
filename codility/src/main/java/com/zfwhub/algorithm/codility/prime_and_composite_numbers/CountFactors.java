1108d97ff926ce63a46568b34d7461ca475f9dbd
markliu2013 <markliu2013@gmail.com>
2018-05-24 12:10:57 +0800

    CountFactors

--- codility/src/main/java/com/zfwhub/algorithm/codility/prime_and_composite_numbers/CountFactors.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/prime_and_composite_numbers/CountFactors.java
@@ -5,7 +5,7 @@ package com.zfwhub.algorithm.codility.prime_and_composite_numbers;
 public class CountFactors {
     
     /**
-     * count 1 by 1 
+     * count 1 by 1  Integer.MAX_VALUE wrong
      */
     public static int solution(int N) {
         int count = 0;
@@ -17,15 +17,85 @@ public class CountFactors {
         return count;
     }
     
-    // TODO CountFactors
+    /**
+     * Don't need count from 1 to n, just need from 1 to n's square root
+     * Will overflow
+     */
     public static int solution2(int N) {
         int count = 0;
-        
+        int i = 1;
+        for (;i * i < N; i++) {
+            if (N % i == 0) {
+                count = count + 2;
+            }
+        }
+        if (i * i == N) {
+			count++;
+		}
+        return count;
+    }
+    
+    public static int solution3(int N) {
+        int count = 0;
+        int i = 1;
+        while (i * i < N) {
+			if (N % i == 0) {
+			    count = count + 2;
+			}
+        	i++;
+		}
+        if (i * i == N) {
+			count++;
+		}
+        return count;
+    }
+    
+    /**
+     * fix overflow  Integer.MAX_VALUE wrong
+     */
+    public static int solution4(int N) {
+    	int count = 0;
+        int i = 1;
+        while (i < Math.sqrt(N)) {
+			if (N % i == 0) {
+			    count = count + 2;
+			}
+        	i++;
+		}
+        if (i == Math.sqrt(N)) {
+			count++;
+		}
+        return count;
+    }
+    
+    // TODO CountFactors fix Integer.MAX_VALUE
+    /**
+     * fix overflow
+     */
+    public static int solution5(long N) {
+        int count = 0;
+        long i = 1L;
+        while (i * i < N) {
+			if (N % i == 0) {
+			    count = count + 2;
+			}
+        	i++;
+		}
+        if (i * i == N) {
+			count++;
+		}
         return count;
     }
     
     public static void main(String[] args) {
-        System.out.println(CountFactors.solution(24));
+//        System.out.println(CountFactors.solution(49));
+//        System.out.println(CountFactors.solution3(49));
+//        System.out.println(CountFactors.solution3(Integer.MAX_VALUE));
+        System.out.println(CountFactors.solution4(49));
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(CountFactors.solution4(Integer.MAX_VALUE));
+        System.out.println(CountFactors.solution5(2147483649L));
+        
     }
 
 }
