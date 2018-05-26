0e79cbd1b352e3ccba28c15634db591bcf4ec129
markliu2013 <markliu2013@gmail.com>
2018-05-26 12:58:48 +0800

    ChocolatesByNumbers

---
+++ codility/src/main/java/com/zfwhub/algorithm/codility/euclidean_algorithm/ChocolatesByNumbers.java
@@ -0,0 +1,25 @@
+package com.zfwhub.algorithm.codility.euclidean_algorithm;
+
+/**
+ * https://app.codility.com/programmers/lessons/12-euclidean_algorithm/chocolates_by_numbers/
+ */
+public class ChocolatesByNumbers {
+    
+    public static int solution(int N, int M) {
+        int count = 0;
+        boolean[] isEmptyArr = new boolean[N];
+        int i = 0;
+        while (isEmptyArr[i] != true) {
+            isEmptyArr[i]  = true;
+            i = (i + M) % N;
+            count++;
+        }
+        isEmptyArr[0] = true;
+        return count;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(ChocolatesByNumbers.solution(10, 4));
+    }
+
+}
