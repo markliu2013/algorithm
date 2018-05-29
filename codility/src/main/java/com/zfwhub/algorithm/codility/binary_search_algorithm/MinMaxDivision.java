c915e760c10d7e54dbc8917b44b5c4261dd36fd5
markliu2013 <markliu2013@gmail.com>
2018-05-29 15:23:34 +0800

    MinMaxDivision

---
+++ codility/src/main/java/com/zfwhub/algorithm/codility/binary_search_algorithm/MinMaxDivision.java
@@ -0,0 +1,21 @@
+package com.zfwhub.algorithm.codility.binary_search_algorithm;
+
+/**
+ * https://app.codility.com/programmers/lessons/14-binary_search_algorithm/min_max_division/
+ */
+public class MinMaxDivision {
+
+    // TODO MinMaxDivision
+    public static int solution(int K, int M, int[] A) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int K = 3;
+        int M = 5;
+        int[] A = new int[] { 2, 1, 5, 1, 2, 2, 2 };
+        System.out.println(MinMaxDivision.solution(K, M, A));
+    }
+
+}
