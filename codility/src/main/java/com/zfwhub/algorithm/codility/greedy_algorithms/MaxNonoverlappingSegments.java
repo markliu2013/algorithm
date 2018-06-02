75925bafad42b0488712bc3156f4a48248a4095d
Mark Liu <markliu2013@gmail.com>
2018-06-02 08:57:33 +0800

    MaxNonoverlappingSegments

---
+++ codility/src/main/java/com/zfwhub/algorithm/codility/greedy_algorithms/MaxNonoverlappingSegments.java
@@ -0,0 +1,18 @@
+package com.zfwhub.algorithm.codility.greedy_algorithms;
+/**
+ * https://app.codility.com/programmers/lessons/16-greedy_algorithms/max_nonoverlapping_segments/
+ */
+public class MaxNonoverlappingSegments {
+    
+    // TODO MaxNonoverlappingSegments
+    public static int solution(int[] A, int[] B) {
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        int[] A = new int[] {1,3,7,9,9};
+        int[] B = new int[] {5,6,8,9,10};
+        System.out.println(MaxNonoverlappingSegments.solution(A, B));
+    }
+
+}
