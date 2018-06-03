a7418961c17a8e4739eb6a8c4a22049c1011eff1
markliu2013 <markliu2013@gmail.com>
2018-06-03 18:34:52 +0800

    MinAbsSum

---
+++ codility/src/main/java/com/zfwhub/algorithm/codility/dynamic_programming/MinAbsSum.java
@@ -0,0 +1,17 @@
+package com.zfwhub.algorithm.codility.dynamic_programming;
+
+/**
+ * https://app.codility.com/programmers/lessons/17-dynamic_programming/min_abs_sum/
+ */
+public class MinAbsSum {
+    
+    public static int solution(int[] A) {
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        int[] A = new int[] {1,5,2,-2};
+        System.out.println(MinAbsSum.solution(A));
+    }
+
+}
