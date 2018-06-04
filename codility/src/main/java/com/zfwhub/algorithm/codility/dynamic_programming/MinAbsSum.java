a595507d5fc368cb5a0c9b133310ddf1fde445bd
markliu2013 <markliu2013@gmail.com>
2018-06-04 23:37:52 +0800

    MinAbsSum

--- codility/src/main/java/com/zfwhub/algorithm/codility/dynamic_programming/MinAbsSum.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/dynamic_programming/MinAbsSum.java
@@ -6,16 +6,25 @@ import java.util.Arrays;
  * https://app.codility.com/programmers/lessons/17-dynamic_programming/min_abs_sum/
  */
 public class MinAbsSum {
-    // TODO MinAbsSum
+    
     public static int solution(int[] A) {
+        int minSubAbsSum = Integer.MAX_VALUE;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = i+1; j < A.length; j++) {
+                int[] subArr = Arrays.copyOfRange(A, i, j);
+                int minAbsSum = minAbsSum(subArr);
+                minSubAbsSum = Math.min(minSubAbsSum, minAbsSum);
+            }
+        }
+        return minSubAbsSum;
+    }
+    
+    public static int minAbsSum(int[] A) {
         if (A.length == 0) {
             return 0;
         }
         if (A.length == 1) {
             return A[0];
-        }
-        if (A.length == 2) {
-            return Math.abs(A[1] - A[0]);
         }
         for (int i = 0; i < A.length; i++) {
             A[i] = Math.abs(A[i]);
@@ -23,26 +32,63 @@ public class MinAbsSum {
         Arrays.sort(A);
         int startIndex = 0;
         int endIndex = A.length - 1;
-        int sum = 0;
-        boolean sumFlag = false;
+        int minAbsSum = A[endIndex] * (-1) + A[startIndex];
         while (startIndex + 1 < endIndex) {
-            sumFlag = false;
-            for (;startIndex < endIndex; startIndex++) {
-                sum += A[startIndex];
-                if (sum >= A[endIndex]) {
-                    sum = sum - A[endIndex];
-                    sumFlag = true;
-                    endIndex--;
-                    break;
+            if (minAbsSum <= 0) {
+                startIndex++;
+                minAbsSum += A[startIndex];
+            } else {
+                endIndex--;
+                minAbsSum -= A[endIndex];
+            }
+        }
+        return Math.abs(minAbsSum);
+    }
+
+    /**
+     * https://stackoverflow.com/questions/44897316/
+     */
+    public static int solution2(int[] A) {
+        if (A.length == 0)
+            return 0;
+        if (A.length == 1)
+            return A[0];
+        int sum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += Math.abs(A[i]);
+        }
+        int[] indices = new int[A.length];
+        indices[0] = 0;
+        int half = sum / 2;
+        int localSum = Math.abs(A[0]);
+        int minLocalSum = Integer.MAX_VALUE;
+        int placeIndex = 1;
+        for (int i = 1; i < A.length; i++) {
+            if (localSum < half) {
+                if (Math.abs(2 * minLocalSum - sum) > Math.abs(2 * localSum - sum))
+                    minLocalSum = localSum;
+                localSum += Math.abs(A[i]);
+                indices[placeIndex++] = i;
+            } else {
+                if (localSum == half)
+                    return Math.abs(2 * half - sum);
+
+                if (Math.abs(2 * minLocalSum - sum) > Math.abs(2 * localSum - sum))
+                    minLocalSum = localSum;
+                if (placeIndex > 1) {
+                    localSum -= Math.abs(A[indices[placeIndex--]]);
+                    i = indices[placeIndex];
                 }
             }
         }
-        return sumFlag ? sum : A[endIndex] - sum;
+        return (Math.abs(2 * minLocalSum - sum));
     }
-    
+
     public static void main(String[] args) {
-        int[] A = new int[] {10, 11, 12, 13, 14};
-        System.out.println(MinAbsSum.solution(A));
+        //        int[] A = new int[] {10, 11, 12, 13, 14};
+        int[] A = new int[] { 1, 5, 2, 5, 2, 3 };
+//        int[] A = new int[] { 1, 2, 2, 3, 5, 5};
+        System.out.println(MinAbsSum.solution2(A));
     }
 
 }
---
+++ codility/src/test/java/com/zfwhub/algorithm/codility/dynamic_programming/MinAbsSumTest.java
@@ -0,0 +1,37 @@
+package com.zfwhub.algorithm.codility.dynamic_programming;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MinAbsSumTest {
+    
+    @Test
+    public void testSolution() {
+        int[] A1 = new int[] {10, 11, 12, 13, 14};
+        int result1 = 6;
+        int[] A2 = new int[] {3, 3, 3, 4, 5};
+        int result2 = 0;
+        int[] A3 = new int[] {};
+        int result3 = 0;
+        int[] A4 = new int[] {3, -4};
+        int result4 = 1;
+        int[] A5 = new int[] {2, 4, 1};
+        int result5 = 1;
+        int[] A6 = new int[] {1, 5, -2, 5, 2, 3};
+        int result6 = 0;
+        int[] A7 = new int[] {1, 5, 2, 5, 2, 3};
+        int result7 = 0;
+        int[] A8 = new int[] {1, 2, 2, 3, 5, 5};
+        int result8 = 2;
+        assertEquals(MinAbsSum.solution(A1), result1);
+        assertEquals(MinAbsSum.solution(A2), result2);
+        assertEquals(MinAbsSum.solution(A3), result3);
+        assertEquals(MinAbsSum.solution(A4), result4);
+        assertEquals(MinAbsSum.solution(A5), result5);
+        assertEquals(MinAbsSum.solution(A6), result6);
+        assertEquals(MinAbsSum.solution(A7), result7);
+        assertEquals(MinAbsSum.solution(A8), result8);
+    }
+
+}
