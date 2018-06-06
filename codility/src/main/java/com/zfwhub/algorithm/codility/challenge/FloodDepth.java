c10865ee069c98a0c296dd15c617514e0b75182a
markliu2013 <markliu2013@gmail.com>
2018-06-07 00:17:03 +0800

    FloodDepth

--- codility/src/main/java/com/zfwhub/algorithm/codility/challenge/FloodDepth.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/challenge/FloodDepth.java
@@ -34,14 +34,58 @@ public class FloodDepth {
         return maximumDepth;
     }
     
-    // TODO FloodDepth
+    /**
+     * dynamic programming
+     */
     public static int solution2(int[] A) {
+        if (A == null || A.length == 0) {
+            throw new RuntimeException("Array A can't be null or empty.");
+        }
+        if (A.length == 1 || A.length == 2) {
+            return 0;
+        }
+        int lowDepthIndex = 0;
+        int highDepthIndex = 0;
+        int currentDept = 0;
+        if (A[0] > A[1]) {
+            lowDepthIndex = 1;
+            currentDept = 0;
+        } else {
+            lowDepthIndex = 1;
+            highDepthIndex = 1;
+            currentDept = 0;
+        }
+        for (int i = 2; i < A.length; i++) {
+            int preHighDepthIndex = highDepthIndex;
+            if (A[i] < A[lowDepthIndex]) {
+                lowDepthIndex = i;
+            }
+            if (A[i] > A[highDepthIndex]) {
+                highDepthIndex = i;
+            }
+            if (A[i] > A[i-1]) {
+                if (A[i] > A[preHighDepthIndex]) {
+                    currentDept = Math.max(A[preHighDepthIndex]-A[lowDepthIndex], currentDept);
+                    lowDepthIndex = i;
+                    highDepthIndex = i;
+                } else {
+                    currentDept = Math.max(A[i]-A[lowDepthIndex], currentDept);
+                }
+            }
+        }
+        return currentDept;
+    }
+    
+    // TODO FloodDepth simply dynamic programming, top to down?
+    public static int solution3(int[] A) {
         return 0;
     }
     
     public static void main(String[] args) {
-        int[] A = new int[] {1,5};
-        System.out.println(FloodDepth.solution(A));
+//        int[] A = new int[] {3,1};
+        int[] A1 = new int[] {1,3,2,1,2,1,5,3,3,4,2};
+        int[] A2 = new int[] {3,1,2};
+        System.out.println(FloodDepth.solution2(A2));
     }
 
 }
