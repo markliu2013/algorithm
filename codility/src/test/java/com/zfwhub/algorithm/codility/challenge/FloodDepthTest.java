bda4445f890d63b42d463c34a3aa2f86f07ba90f
markliu2013 <markliu2013@gmail.com>
2018-06-07 00:17:03 +0800

    FloodDepth

--- codility/src/test/java/com/zfwhub/algorithm/codility/challenge/FloodDepthTest.java
+++ codility/src/test/java/com/zfwhub/algorithm/codility/challenge/FloodDepthTest.java
@@ -12,8 +12,24 @@ public class FloodDepthTest {
         int result1 = 2;
         int[] A2 = new int[] {5,8};
         int result2 = 0;
+        int[] A3 = new int[] {3,1,2};
+        int result3 = 1;
         assertEquals(FloodDepth.solution(A1), result1);
         assertEquals(FloodDepth.solution(A2), result2);
+        assertEquals(FloodDepth.solution(A3), result3);
+    }
+    
+    @Test
+    public void testSolution2() {
+        int[] A1 = new int[] {1,3,2,1,2,1,5,3,3,4,2};
+        int result1 = 2;
+        int[] A2 = new int[] {5,8};
+        int result2 = 0;
+        int[] A3 = new int[] {3,1,2};
+        int result3 = 1;
+        assertEquals(FloodDepth.solution2(A1), result1);
+        assertEquals(FloodDepth.solution2(A2), result2);
+        assertEquals(FloodDepth.solution2(A3), result3);
     }
 
 }
