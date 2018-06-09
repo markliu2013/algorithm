5cabe2c9f6205725e2ac41b869b82970fc3a5fd9
markliu2013 <markliu2013@gmail.com>
2018-06-09 22:07:01 +0800

    AddTwoNumbers

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/math/AddTwoNumbers.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/math/AddTwoNumbers.java
@@ -23,7 +23,6 @@ public class AddTwoNumbers {
      * like how you would sum two numbers on a piece of paper
      * http://www.aaamath.com/add27dx1.htm
      */
-    // TODO
     public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
         ListNode result = null;
         ListNode currentNode1 = l1;
@@ -31,7 +30,7 @@ public class AddTwoNumbers {
         int remainVal = 0;
         int currentVal = currentNode1.val + currentNode2.val;
         if (currentVal > 9) {
-            currentVal = 10 - currentVal;
+            currentVal = currentVal - 10;
             remainVal = 1;
         }
         result = new ListNode(currentVal);
@@ -40,28 +39,56 @@ public class AddTwoNumbers {
             l3 = new ListNode(remainVal);
             result.next = l3;
         }
-        while (l1.next != null || l2.next != null) {
+        ListNode currentNode = result;
+        while (currentNode1.next != null || currentNode2.next != null) {
             int currentVal1 = 0;
             int currentVal2 = 0;
-            if (l1.next != null) {
-                currentNode1 = l1.next;
+            if (currentNode1.next != null) {
+                currentNode1 = currentNode1.next;
                 currentVal1 = currentNode1.val;
             }
-            if (l2.next != null) {
-                currentNode2 = l2.next;
+            if (currentNode2.next != null) {
+                currentNode2 = currentNode2.next;
                 currentVal2 = currentNode2.val;
             }
-            currentVal = currentNode1.val + currentNode2.val;
+            currentVal = remainVal + currentVal1 + currentVal2;
             if (currentVal > 9) {
-                currentVal = 10 - currentVal;
+                currentVal = currentVal - 10;
                 remainVal = 1;
             } else {
                 remainVal = 0;
             }
             ListNode l4 = new ListNode(currentVal);
-            
+            currentNode.next = l4;
+            currentNode = currentNode.next;
+            if (remainVal != 0) {
+                l4.next = new ListNode(remainVal);
+            }
         }
         return result;
+    }
+    
+    /**
+     * https://leetcode.com/problems/add-two-numbers/solution/
+     */
+    public ListNode addTwoNumbers3(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
     }
 
     public BigInteger parseNodeToInter(ListNode node) {
@@ -82,7 +109,7 @@ public class AddTwoNumbers {
         return node;
     }
 
-    public String parseNodetoLinkedString(ListNode node) {
+    public static String parseNodetoLinkedString(ListNode node) {
         ListNode current = node;
         List<String> nodes = new ArrayList<String>();
         while (current != null) {
@@ -102,9 +129,10 @@ public class AddTwoNumbers {
     
     public static void main(String[] args) {
         AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
-        ListNode l1 = addTwoNumbers.parseIntegerToNode(new BigInteger("243"));
-        ListNode l2 = addTwoNumbers.parseIntegerToNode(new BigInteger("564"));
-        System.out.println(addTwoNumbers.parseNodeToInter(addTwoNumbers.addTwoNumbers2(l1, l2)));
+        ListNode l1 = addTwoNumbers.parseIntegerToNode(new BigInteger("342"));
+        ListNode l2 = addTwoNumbers.parseIntegerToNode(new BigInteger("465"));
+        System.out.println(parseNodetoLinkedString(addTwoNumbers.addTwoNumbers3(l1, l2)));
+        System.out.println(parseNodetoLinkedString(addTwoNumbers.addTwoNumbers2(l1, l2)));
     }
 
 }
