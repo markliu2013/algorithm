cc5cdda8aeca4ec5f9e791bea105780b701ac2f6
markliu2013 <markliu2013@gmail.com>
2018-06-12 10:47:08 +0800

    ACM ABProblem

---
+++ acm/src/main/java/com/zfwhub/algorithm/acm/math/ABProblem.java
@@ -0,0 +1,14 @@
+package com.zfwhub.algorithm.acm.math;
+
+import java.io.*;
+import java.util.*;
+
+public class ABProblem {
+
+    public static void main(String args[]) throws Exception {
+        Scanner cin = new Scanner(System.in);
+        int a = cin.nextInt(), b = cin.nextInt();
+        System.out.println(a + b);
+    }
+
+}
