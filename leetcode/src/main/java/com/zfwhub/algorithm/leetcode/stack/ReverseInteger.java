5a2b75651457fe37500b0d136dd71a2a8559cacc
markliu2013 <markliu2013@gmail.com>
2018-06-15 08:59:09 +0800

    ReverseInteger

---
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/stack/ReverseInteger.java
@@ -0,0 +1,26 @@
+package com.zfwhub.algorithm.leetcode.stack;
+
+/**
+ * https://leetcode.com/problems/reverse-integer/description/
+ */
+public class ReverseInteger {
+
+    public static int solution(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7))
+                return 0;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8))
+                return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ReverseInteger.solution(-123));
+    }
+
+}
