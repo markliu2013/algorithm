b9a4ca5064ed9765fc787d667c52bab4f73c6018
Mark Liu <markliu2013@gmail.com>
2018-06-27 20:54:58 +0800

    MinimumSwapsToMakeSequencesIncreasing Failed

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/array/MinimumSwapsToMakeSequencesIncreasing.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/array/MinimumSwapsToMakeSequencesIncreasing.java
@@ -10,39 +10,50 @@ public class MinimumSwapsToMakeSequencesIncreasing {
     private static int[] newA = null;
     private static int[] newB = null;
     
-    // TODO MinimumSwapsToMakeSequencesIncreasing wrong
-    public static int solution(int[] A, int[] B) {
+    private static int swapCount = 0;
+    private static boolean lastSwaped = false;
+    
+    public static void solution(int[] A, int[] B) {
         if (A.length == 1) {
-            return 0;
+            lastSwaped = false;
+            return;
         }
-        if (A.length == 2) {
-            if (A[1] <= A[0] || B[1] <= B[0]) {
-                return 1;
+        if (A[A.length-1] <= newA[A.length-2] || B[B.length-1] <= newB[B.length-2]) {
+            if (lastSwaped == false) {
+                int temp = A[A.length-1];
+                newA[A.length-1] = B[B.length-1];
+                newB[B.length-1] = temp;
+                swapCount++;
             } else {
-                return 0;
+                int temp1 = A[A.length-1];
+                newA[A.length-1] = B[B.length-1];
+                newB[B.length-1] = temp1;
+                // reverse back
+                int temp2 = A[A.length-2];
+                newA[A.length] = B[B.length];
+                newB[B.length] = temp2;
             }
+            lastSwaped = true;
         }
         int[] subA = Arrays.copyOfRange(A, 0, A.length-1);
         int[] subB = Arrays.copyOfRange(B, 0, B.length-1);
-        if (A[A.length-1] >= A[A.length-2]) {
-            
-        }
-        
-        
-        
-        return 0;
+        solution(subA, subB);
+    }
+    
+    public static int minSwap(int[] A, int[] B) {
+        newA = Arrays.copyOfRange(A, 0, A.length);
+        newB = Arrays.copyOfRange(B, 0, B.length);
+        solution(A, B);
+        return swapCount;
     }
     
     public static void main(String[] args) {
         int[] A2 = new int[] {3,3,8,9,10};
         int[] B2 = new int[] {1,7,4,6,8};
-        newA = Arrays.copyOfRange(A2, 0, A2.length);
-        newB = Arrays.copyOfRange(B2, 0, B2.length);
-//        System.out.println(MinimumSwapsToMakeSequencesIncreasing.solution(A2, B2));
         
-        System.out.println(1-Math.pow(0.997, 100));
-        
-        
+        System.out.println(MinimumSwapsToMakeSequencesIncreasing.minSwap(A2, B2));
+        System.out.println(Arrays.toString(newA));
+        System.out.println(Arrays.toString(newB));
     }
 
 }
