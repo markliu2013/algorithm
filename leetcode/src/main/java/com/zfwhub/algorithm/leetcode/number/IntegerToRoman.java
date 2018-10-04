41ca263abff7f26d4ee1ef2f93b4217691a3ac97
Mark Liu <markliu2013@gmail.com>
2018-10-04 13:32:43 +0800

    IntegerToRomanubstring

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/number/IntegerToRoman.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/number/IntegerToRoman.java
@@ -4,9 +4,24 @@ package com.zfwhub.algorithm.leetcode.number;
  * https://leetcode.com/problems/integer-to-roman/description/
  */
 public class IntegerToRoman {
-    
+
     public static String solution(int num) {
-        return "";
+        String[][] c = {
+            { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" },
+            { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" },
+            { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" },
+            { "", "M", "MM", "MMM"}
+        };
+        StringBuilder roman = new StringBuilder();
+        roman.append(c[3][num / 1000 % 10]);
+        roman.append(c[2][num / 100 % 10]);
+        roman.append(c[1][num / 10 % 10]);
+        roman.append(c[0][num % 10]);
+        return roman.toString();
     }
-    
+
+    public static void main(String[] args) {
+        System.out.println(IntegerToRoman.solution(1994));
+    }
+
 }
