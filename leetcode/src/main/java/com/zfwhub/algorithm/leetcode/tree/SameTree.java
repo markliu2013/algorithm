973b8592c91d00824befdeeb3dcb2c5ab62866e4
Mark Liu <markliu2013@gmail.com>
2018-10-19 15:24:28 +0800

    SameTree

---
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/tree/SameTree.java
@@ -0,0 +1,16 @@
+package com.zfwhub.algorithm.leetcode.tree;
+
+/**
+ * https://leetcode.com/problems/same-tree/
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return false;
+    }
+}
