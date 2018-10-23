46128589ce68349d87f53faa76ab185fd23e4728
Mark Liu <markliu2013@gmail.com>
2018-10-23 18:41:19 +0800

    TreeUtil

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/tree/TreeUtil.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/tree/TreeUtil.java
@@ -88,7 +88,7 @@ public class TreeUtil {
     }
 
     public static void main(String[] args) {
-        TreeNode node = createLevelOrder(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        TreeNode node = createLevelOrder(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
         System.out.println(preOrderTraverse(node));
     }
 
