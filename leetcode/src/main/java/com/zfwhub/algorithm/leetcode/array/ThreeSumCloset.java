5f40b68600c7f5556fe12c15931817e71a28ad67
Mark Liu <markliu2013@gmail.com>
2018-10-24 14:36:42 +0800

    ThreeSumCloset

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/array/ThreeSumCloset.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/array/ThreeSumCloset.java
@@ -3,22 +3,37 @@ package com.zfwhub.algorithm.leetcode.array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * https://leetcode.com/problems/3sum-closest/
  */
 public class ThreeSumCloset {
-    
+
     public int solution(int[] nums, int target) {
+        int result = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int gap = Math.abs(nums[i] + nums[j] + nums[k] - target);
+                    if (gap < min) {
+                        min = gap;
+                        result = nums[i] + nums[j] + nums[k];
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public int solution2(int[] nums, int target) {
         // 优化数组，删除出现超过三次的元素
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i])+1);
+                map.put(nums[i], map.get(nums[i]) + 1);
             } else {
                 map.put(nums[i], 1);
             }
@@ -31,8 +46,7 @@ public class ThreeSumCloset {
             }
         }
         Collections.sort(list2);
-        
-        
+
         return 0;
     }
 
