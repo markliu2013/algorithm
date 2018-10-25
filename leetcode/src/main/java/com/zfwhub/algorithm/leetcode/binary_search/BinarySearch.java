2e38ee1cce7de2488f4556c90f75f0b16d8f638f
markliu2013 <markliu2013@gmail.com>
2018-10-25 21:43:15 +0800

    BinarySearch

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/binary_search/BinarySearch.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/binary_search/BinarySearch.java
@@ -15,12 +15,15 @@ public class BinarySearch {
     }
     
     public static int solution2(int[] nums, int target) {
-        int mid = nums.length / 2;
-        while (mid > 0 && mid < nums.length) {
+        int startIndex = 0;
+        int endIndex = nums.length-1;
+        int mid = (startIndex + endIndex) / 2;
+        while (startIndex <= endIndex) {
+            mid = (startIndex + endIndex) / 2;
             if (nums[mid] > target) {
-                mid = mid / 2;
+                endIndex = mid-1;
             } else if (nums[mid] < target) {
-                mid = (mid + nums.length) / 2;
+                startIndex = mid+1;
             } else {
                 return mid;
             }
@@ -33,10 +36,14 @@ public class BinarySearch {
         int target = 9;
         int[] nums1 = new int[] {-1,0,3,5,9,12};
         int target1 = 2;
+        int[] nums2 = new int[] {5};
+        int target2 = 5;
         System.out.println(solution(nums, target));
-       // System.out.println(solution2(nums, target));
+        System.out.println(solution2(nums, target));
         System.out.println(solution(nums1, target1));
         System.out.println(solution2(nums1, target1));
+        System.out.println(solution(nums2, target2));
+        System.out.println(solution2(nums2, target2));
     }
 
 }
