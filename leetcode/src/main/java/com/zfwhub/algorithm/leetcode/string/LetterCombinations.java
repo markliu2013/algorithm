fd73f580273362a8108feab34d23e607af639054
Mark Liu <markliu2013@gmail.com>
2018-10-30 17:14:25 +0800

    LetterCombinations

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/string/LetterCombinations.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/string/LetterCombinations.java
@@ -5,23 +5,29 @@ import java.util.*;
  */
 public class LetterCombinations {
     
-    public static Map<String, String> map;
+    public static Map<Character, String> map;
     
     static {
         map = new HashMap<>();
-        map.put("2", "abc");
-        map.put("3", "def");
-        map.put("4", "ghi");
-        map.put("5", "jkl");
-        map.put("6", "mno");
-        map.put("7", "pqrs");
-        map.put("8", "tuv");
-        map.put("9", "wxyz");
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
     }
     
     public static List<String> solution(String digits) {
-        System.out.println(map);
-        return null;
+        Set<String> set = new HashSet<>();
+        List<String> list1 = new ArrayList<>();
+        for (int i = 0; i < digits.length(); i++) {
+            list1.add(map.get(digits.charAt(i)));
+        }
+        
+        List<String> list = new ArrayList<>(set);
+        return list;
     }
     
     public static void main(String[] args) {
