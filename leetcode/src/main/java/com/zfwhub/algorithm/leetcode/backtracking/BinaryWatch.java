61d405416a2edac34d81cd70554ddce5d57bf52e
Mark Liu <markliu2013@gmail.com>
2018-11-01 18:51:09 +0800

    BinaryWatch

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/BinaryWatch.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/BinaryWatch.java
@@ -8,15 +8,39 @@ import java.util.*;
 public class BinaryWatch {
     
     public static Integer[] arr = new Integer[] {60, 60*2, 60*4, 60*8, 1, 2, 4, 8, 16, 32};
+    public static int maxMinutes = 11*60 + 59;
     
+    // wrong, 不能同时选择4个分针，比如n取4，选择 4, 8, 16, 32
     public static List<String> solution(int num) {
+        List<String> result = new ArrayList<>();
         List<List<Integer>> list = getCombination4(arr, num);
-        return null;
+        List<Integer> minutes = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            List<Integer> list1 = list.get(i);
+            int mins = 0;
+            for (int j = 0; j < list1.size(); j++) {
+                mins += list1.get(j);
+            }
+            if (mins <= maxMinutes) {
+                minutes.add(mins);
+            }
+        }
+        Collections.sort(minutes);
+        for (int i = 0; i < minutes.size(); i++) {
+            result.add(minToTime(minutes.get(i)));
+        }
+        return result;
     }
     
     
     private static String minToTime(int mins) {
-        return "";
+        int hour = mins / 60;
+        int min = mins % 60;
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.valueOf(hour));
+        sb.append(":");
+        sb.append(String.format("%2s", min).replace(' ', '0'));
+        return sb.toString();
     }
     
     public static List<List<Integer>> getCombination4(Integer[] arr, int n) {
@@ -42,6 +66,10 @@ public class BinaryWatch {
             stack.pop();
             n++;
         }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(solution(2));
     }
     
 }
