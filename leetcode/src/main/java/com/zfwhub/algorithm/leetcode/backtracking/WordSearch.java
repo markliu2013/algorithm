971b85851d06daec91b9f2e2306a59bcba221664
Mark Liu <markliu2013@gmail.com>
2018-11-05 17:10:59 +0800

    WordSearch

---
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/WordSearch.java
@@ -0,0 +1,12 @@
+package com.zfwhub.algorithm.leetcode.backtracking;
+
+/**
+ * https://leetcode.com/problems/word-search/ 
+ */
+public class WordSearch {
+    
+    public static boolean exist(char[][] board, String word) {
+        return false;
+    }
+
+}
