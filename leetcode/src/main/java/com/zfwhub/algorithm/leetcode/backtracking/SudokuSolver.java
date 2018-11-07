0af5b8a93e231e2873481aeae4a350a86bc27926
Mark Liu <markliu2013@gmail.com>
2018-11-07 16:23:14 +0800

    SudokuSolver

---
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/SudokuSolver.java
@@ -0,0 +1,10 @@
+package com.zfwhub.algorithm.leetcode.backtracking;
+
+// https://leetcode.com/problems/sudoku-solver/
+public class SudokuSolver {
+    
+    public static void solveSudoku(char[][] board) {
+        
+    }
+    
+}
