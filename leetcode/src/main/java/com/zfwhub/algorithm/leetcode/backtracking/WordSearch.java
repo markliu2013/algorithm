c0ad6d210bb4307e4a8cb7447c2c7429fa34237d
Mark Liu <markliu2013@gmail.com>
2018-11-09 22:16:31 +0800

    WordSearch

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/WordSearch.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/WordSearch.java
@@ -7,14 +7,17 @@ import java.util.*;
  */
 public class WordSearch {
     
+    public static int k = 0; // k 代表当前word的index
+    
     public static boolean exist(char[][] board, String word) {
         List<List<List<Integer>>> result = new ArrayList<>();
         List<List<Integer>> solution = new ArrayList<>();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j] == word.charAt(0)) {//入口
+                    k = 0;
                     makeMove(solution, i, j);
-                    dfs(result, solution, board, word, i, j, 0);
+                    dfs(result, solution, board, word, i, j);
                 }
             }
         }
@@ -22,21 +25,21 @@ public class WordSearch {
     }
     
     // i,j 二维数组搜索入口，k是word的索引。
-    public static void dfs(List<List<List<Integer>>> result, List<List<Integer>> solution, char[][] board, String word, int i, int j, int k) {
+    public static void dfs(List<List<List<Integer>>> result, List<List<Integer>> solution, char[][] board, String word, int i, int j) {
         if (isASolution(solution, word)) {
             processSolution(result, solution);
             return;
         }
-        while (k < word.length()-1) {
-            k = k + 1;
+        
+        // 每一个位置，都上下左右尝试。
             //上
             if (i > 0) {
                 i = i - 1;
-                if (isValid(board, word, i, j, k, solution)) {
+                if (isValid(board, word, i, j, solution)) {
                     makeMove(solution, i, j);
-                    dfs(result, solution, board, word, i, j, k);
+                    dfs(result, solution, board, word, i, j);
                     unmakeMove(solution);
-                    continue;
+                 //   continue;
                 } else {
                     i = i + 1;
                 }
@@ -45,11 +48,11 @@ public class WordSearch {
             //下
             if (i < board.length - 1) {
                 i = i + 1;
-                if (isValid(board, word, i, j, k, solution)) {
+                if (isValid(board, word, i, j, solution)) {
                     makeMove(solution, i, j);
-                    dfs(result, solution, board, word, i, j, k);
+                    dfs(result, solution, board, word, i, j);
                     unmakeMove(solution);
-                    continue;
+                  //  continue;
                 } else {
                     i = i - 1;
                 }
@@ -57,11 +60,11 @@ public class WordSearch {
             //左
             if (j > 0) {
                 j = j - 1;
-                if (isValid(board, word, i, j, k, solution)) {
+                if (isValid(board, word, i, j, solution)) {
                     makeMove(solution, i, j);
-                    dfs(result, solution, board, word, i, j, k);
+                    dfs(result, solution, board, word, i, j);
                     unmakeMove(solution);
-                    continue;
+                   // continue;
                 } else {
                     j = j + 1;
                 }
@@ -69,17 +72,17 @@ public class WordSearch {
             //右
             if (j < board[i].length - 1) {
                 j = j + 1;
-                if (isValid(board, word, i, j, k, solution)) {
+                if (isValid(board, word, i, j, solution)) {
                     makeMove(solution, i, j);
-                    dfs(result, solution, board, word, i, j, k);
+                    dfs(result, solution, board, word, i, j);
                     unmakeMove(solution);
-                    continue;
+                    //continue;
                 } else {
                     j = j - 1;
                 }
             }
         }
-    }
+    
     
     public static boolean isASolution(List<List<Integer>> solution, String word) {
         return solution.size() == word.length();
@@ -89,7 +92,7 @@ public class WordSearch {
         result.add(new ArrayList<>(solution));
     }
     // 检查是否相等，且不能重复走
-    public static boolean isValid(char[][] board, String word, int i, int j, int k, List<List<Integer>> solution) {
+    public static boolean isValid(char[][] board, String word, int i, int j, List<List<Integer>> solution) {
         List<Integer> step = new ArrayList<>();
         step.add(i);
         step.add(j);
@@ -103,9 +106,11 @@ public class WordSearch {
         step.add(i);
         step.add(j);
         solution.add(step);
+        k = k + 1;
     }
     public static void unmakeMove(List<List<Integer>> solution) {
         solution.remove(solution.size()-1);
+        k = k - 1;
     }
     public static void main(String[] args) {
         char[][] board = {
@@ -124,7 +129,7 @@ public class WordSearch {
                 {'S','F','E','S'},
                 {'A','D','E','E'}
             };
-        String word4 = "ABCESEEEFS";
+        String word4 = "ABCESEE";
         System.out.println(exist(board1, word4));
     }
 
