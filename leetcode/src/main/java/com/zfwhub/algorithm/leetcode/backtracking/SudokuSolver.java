5b1e709e6a8b1b3b44e20db1104da2b1cebec758
markliu2013 <markliu2013@gmail.com>
2018-11-10 23:46:46 +0800

    SudokuSolver

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/SudokuSolver.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/SudokuSolver.java
@@ -2,8 +2,6 @@ package com.zfwhub.algorithm.leetcode.backtracking;
 
 import java.util.*;
 
-import javax.swing.border.Border;
-
 // https://leetcode.com/problems/sudoku-solver/
 public class SudokuSolver {
     
@@ -11,22 +9,48 @@ public class SudokuSolver {
     public static final int RANK = 9;
     
     public static void solveSudoku(char[][] board) {
-        dfs(board);
+        List<Character> solution = new ArrayList<>();//每一个格子都是数字几
+        dfs(board, solution);
     }
-    public static void dfs(char[][] board) {
-        if (isASolution()) {
-            processSolution();
-            return;
+    public static void dfs(char[][] board, List<Character> solution) {
+        if (isASolution(solution)) {
+            processSolution(board, solution);
+        } else {
+            // 每一个格子都尝试1-9
+            for (int i = 1; i <= RANK; i++) {
+                if (isValid(board, solution, i)) {
+                    makeMove(solution, i);
+                    dfs(board, solution);
+                    unMakeMove(solution);
+                }
+            }
         }
         
     }
-    public static boolean isASolution() {
-        return false;
+    public static boolean isASolution(List<Character> solution) {
+        return solution.size() == RANK * RANK;
     }
-    public static void processSolution() {
+    public static void processSolution(char[][] board, List<Character> solution) {
+        for (int i = 0; i < solution.size(); i++) {
+            board[i/9][i%9] = solution.get(i);
+        }
+    }
+    public static boolean isValid(char[][] oriBoard, List<Character> solution, int k) {
+        // init the board to check
+        char[][] board = new char[9][9];
+        for (int i = 0; i < oriBoard.length; i++) {
+            for (int j = 0; j < oriBoard[i].length; j++) {
+                board[i][j] = oriBoard[i][j];
+            }
+        }
+        for (int i = 0; i < solution.size(); i++) {
+            board[i/9][i%9] = solution.get(i);
+        }
+        if (board[solution.size()/9][solution.size()%9] != EMPTY_CHAR && board[solution.size()/9][solution.size()%9] != Character.forDigit(k, 10)) {
+            return false;
+        }
+        board[solution.size()/9][solution.size()%9] = Character.forDigit(k, 10);
         
-    }
-    public static boolean isValid(char[][] board) {
         // Each of the digits 1-9 must occur exactly once in each row.
         for (int i = 0; i < RANK; i++) {
             Set<Character> set = new HashSet<>();
@@ -64,12 +88,12 @@ public class SudokuSolver {
         }
         return true;
     }
-    public static void unmakeMove() {
-       
+    public static void unMakeMove(List<Character> solution) {
+        solution.remove(solution.size()-1);
     }
 
-    public static void makeMove() {
-        
+    public static void makeMove(List<Character> solution, int i) {
+        solution.add(Character.forDigit(i, 10));
     }
     
     // find the number closest to n and divisible by m
@@ -98,7 +122,6 @@ public class SudokuSolver {
             }
             System.out.println();
         }
-       // System.out.println(closestNumber(7,3));
     }
     
 }
