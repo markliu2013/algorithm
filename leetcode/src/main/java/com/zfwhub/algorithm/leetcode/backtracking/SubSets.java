733196e41183b27f376ffa5a1681c7defb667cbc
markliu2013 <markliu2013@gmail.com>
2018-11-10 22:16:09 +0800

    SubSets

---
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/SubSets.java
@@ -0,0 +1,21 @@
+package com.zfwhub.algorithm.leetcode.backtracking;
+import java.util.*;
+// https://leetcode.com/problems/subsets/
+// https://leetcode.com/problems/subsets/discuss/27281/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)
+public class SubSets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+        list.add(new ArrayList<>(tempList));
+        for(int i = start; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
