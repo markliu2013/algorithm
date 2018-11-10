43d25d9a247e617d7a6452ddfc20eb6ad3c592ba
Mark Liu <markliu2013@gmail.com>
2018-11-10 16:39:21 +0800

    SudokuSolver

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/SudokuSolver.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/SudokuSolver.java
@@ -2,8 +2,13 @@ package com.zfwhub.algorithm.leetcode.backtracking;
 
 import java.util.*;
 
+import javax.swing.border.Border;
+
 // https://leetcode.com/problems/sudoku-solver/
 public class SudokuSolver {
+    
+    public static final char EMPTY_CHAR = '.'; 
+    public static final int RANK = 9;
     
     public static void solveSudoku(char[][] board) {
         dfs(board);
@@ -21,7 +26,42 @@ public class SudokuSolver {
     public static void processSolution() {
         
     }
-    public static boolean isValid() {
+    public static boolean isValid(char[][] board) {
+        // Each of the digits 1-9 must occur exactly once in each row.
+        for (int i = 0; i < RANK; i++) {
+            Set<Character> set = new HashSet<>();
+            for (int j = 0; j < RANK; j++) {
+                if (board[i][j] != EMPTY_CHAR && set.contains(board[i][j])) {
+                    return false;
+                } else {
+                    set.add(board[i][j]);
+                }
+            }
+        }
+        // Each of the digits 1-9 must occur exactly once in each column.
+        for (int i = 0; i < RANK; i++) {
+            Set<Character> set = new HashSet<>();
+            for (int j = 0; j < RANK; j++) {
+                if (board[j][i] != EMPTY_CHAR && set.contains(board[j][i])) {
+                    return false;
+                } else {
+                    set.add(board[j][i]);
+                }
+            }
+        }
+        // Each of the the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+        for (int i = 0; i < RANK; i++) {
+            Set<Character> set = new HashSet<>();
+            for (int j = 0; j < RANK; j++) {
+                int row = closestNumber(i,3)+j/3;
+                int col = j%3+(i%3)*3;
+                if (board[row][col] != EMPTY_CHAR && set.contains(board[row][col])) {
+                    return false;
+                } else {
+                    set.add(board[row][col]);
+                }
+            }
+        }
         return true;
     }
     public static void unmakeMove() {
@@ -30,6 +70,14 @@ public class SudokuSolver {
 
     public static void makeMove() {
         
+    }
+    
+    // find the number closest to n and divisible by m
+    // https://www.geeksforgeeks.org/find-number-closest-n-divisible-m/
+    public static int closestNumber(int n, int m) {
+        int q = n / m; 
+        int n1 = m * q; 
+        return n1; 
     }
     public static void main(String[] args) {
         char[][] board = {
@@ -44,12 +92,13 @@ public class SudokuSolver {
                 {'.','.','.','.','8','.','.','7','9'},
         };
         solveSudoku(board);
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
+        for (int i = 0; i < RANK; i++) {
+            for (int j = 0; j < RANK; j++) {
                 System.out.print(board[i][j]+" ");
             }
             System.out.println();
         }
+       // System.out.println(closestNumber(7,3));
     }
     
 }
