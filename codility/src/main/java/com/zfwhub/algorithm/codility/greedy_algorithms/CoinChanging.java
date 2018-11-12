ca105d0aebe44d0a09270d6cb91ccdee40635c73
Mark Liu <markliu2013@gmail.com>
2018-11-12 16:53:09 +0800

    CoinChanging

--- codility/src/main/java/com/zfwhub/algorithm/codility/greedy_algorithms/CoinChanging.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/greedy_algorithms/CoinChanging.java
@@ -1,5 +1,6 @@
 package com.zfwhub.algorithm.codility.greedy_algorithms;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -24,14 +25,47 @@ public class CoinChanging {
     // TODO CoinChanging
     // https://codility.com/media/train/15-DynamicProgramming.pdf
     public static LinkedHashMap<Integer, Integer> solution2(int[] M, int k) {
-        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<Integer, Integer>();
-        Map<Integer, Integer> dp = new HashMap<>();
+        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+        System.out.println(dp(M, k, map));
         return map;
+    }
+    
+    public static int dp(int[] M, int k, LinkedHashMap<Integer, Integer> map) {
+        if (k == 0) {
+            return 0;
+        }
+        if (M.length == 1) {
+            if (k % M[0] == 0) {
+                return k / M[0];
+            } else {
+                return -1;
+            }
+        }
+        int[] subM = Arrays.copyOfRange(M, 0, M.length-1);
+        // if the amount to be paid is smaller than the highest denomination, this denomination can be discarded.
+        if (k < M[M.length-1]) {
+            return dp(subM, k, map);
+        }
+        // use highest denomination
+        int count1 = dp(subM, k % M[M.length-1], map) + k / M[M.length-1];
+        // dont use highest denomination
+        int count2 = dp(subM, k, map);
+        return Math.min(count1, count2);
+    }
+    
+    // 计算用了多少coins
+    public static int count(LinkedHashMap<Integer, Integer> map) {
+        int count =  0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            count += entry.getValue();
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         int[] M = new int[] { 1, 3, 4 };
         int k = 6;
         System.out.println(CoinChanging.solution(M, k));
+        System.out.println(CoinChanging.solution2(M, k));
     }
 }
--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/CoinChange.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/CoinChange.java
@@ -1,7 +1,68 @@
 package com.zfwhub.algorithm.leetcode.dp;
 
+import java.util.*;
+
 // https://leetcode.com/problems/coin-change/
 // https://www.cnblogs.com/grandyang/p/5138186.html
 public class CoinChange {
-
+    
+    public static int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        return dp(coins, amount);
+    }
+    
+    public static int dp(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+        if (coins.length == 0) {
+            return -1;
+        }
+        if (coins.length == 1) {
+            if (amount % coins[0] == 0) {
+                return amount / coins[0];
+            } else {
+                return -1;
+            }
+        }
+        int lastCoin = coins[coins.length-1];
+        int[] subCoins = Arrays.copyOfRange(coins, 0, coins.length-1);
+        // if the amount to be paid is smaller than the highest denomination, this denomination can be discarded.
+        if (amount < lastCoin) {
+            return dp(subCoins, amount);
+        }
+        
+        int maxHighestCount = amount / lastCoin;//最多用多少次
+        int minCount = Integer.MAX_VALUE;
+        for (int i = maxHighestCount; i >= 1; i--) {
+            int count1 = dp(subCoins, ((amount % lastCoin) + (lastCoin*(maxHighestCount-i)))) + i;
+            int count2 = dp(subCoins, amount-(lastCoin*(maxHighestCount-i)))+(maxHighestCount-i);
+            int count = -1;
+            if (Math.min(count1, count2) == -1) {
+                if (count1 == count2 && count1 == -1) {
+                    count = -1;
+                } else {
+                    count = Math.max(count1, count2);
+                }
+            } else {
+                count = Math.min(count1, count2);
+            }
+            minCount = Math.min(minCount, count);
+        }
+        return minCount;
+    }
+    public static void main(String[] args) {
+//        int[] coins1 = new int[] { 1, 2, 5 };
+//        int amount1 = 11;
+//        System.out.println(coinChange(coins1, amount1));
+//        int[] coins2 = new int[] { 2 };
+//        int amount2 = 3;
+//        System.out.println(coinChange(coins2, amount2));
+//        int[] coins3 = new int[] {  2, 5, 10 , 1 };
+//        int amount3 = 27;
+//        System.out.println(coinChange(coins3, amount3));
+        int[] coins4 = new int[] { 83, 186, 408, 419 };
+        int amount4 = 6249;
+        System.out.println(coinChange(coins4, amount4));
+    }
 }
