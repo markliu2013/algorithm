9eb4143ccbdae0ad6f8a6a8d88ef00a6d20e4d69
Mark Liu <markliu2013@gmail.com>
2018-11-20 21:57:31 +0800

    StickersToSpellWord

---
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/StickersToSpellWord.java
@@ -0,0 +1,10 @@
+package com.zfwhub.algorithm.leetcode.dp;
+
+// https://leetcode.com/problems/stickers-to-spell-word/
+public class StickersToSpellWord {
+    
+    public static int minStickers(String[] stickers, String target) {
+        return 0;
+    }
+    
+}
