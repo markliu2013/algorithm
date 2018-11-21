81d4d7a1b1b4ed87a30a1077356b6d8341fac0b7
Mark Liu <markliu2013@gmail.com>
2018-11-21 20:01:37 +0800

    Climbingways

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/ClimbingWays.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/ClimbingWays.java
@@ -1,6 +1,6 @@
 package com.zfwhub.algorithm.leetcode.dp;
 
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * Count ways to reach the n’th stair
@@ -69,9 +69,56 @@ public class ClimbingWays {
         }
         return temp;
     }
+    
+    public static List<List<Integer>> getClimbingWays4(int n) {
+        List<List<Integer>> solutionList = new ArrayList<>();
+        // 存储1和2，代表每次走多少步。
+        List<Integer> solution = new ArrayList<>();
+        dfs(solutionList, solution, n, 0);
+        return solutionList;
+    }
+    
+    // c当前在哪一级
+    public static void dfs(List<List<Integer>> solutionList, List<Integer> solution, int n, int c) {
+        if (isASolution(solution, n, c)) {
+            processSolution(solutionList, solution);
+            return;
+        }
+        // 走一步或两步
+        for (int i = 1; i <= 2; i++) {
+            if (isValid(solution, i)) {
+                makeMove(solution, i);
+                dfs(solutionList, solution, n, c+i);
+                unMakeMove(solution);
+            }
+        }
+    }
+    
+    public static boolean isASolution(List<Integer> solution, int n, int c) {
+        System.out.println(solution);
+        return c == n;
+    }
+
+    public static void processSolution(List<List<Integer>> solutionList, List<Integer> solution) {
+        solutionList.add(new ArrayList<>(solution));
+    }
+    
+    public static boolean isValid(List<Integer> solution, int n) {
+        return solution.stream().mapToInt(Integer::intValue).sum() <= n;
+    }
+    
+    public static void makeMove(List<Integer> solution, int n) {
+        solution.add(n);
+    }
+    
+    public static void unMakeMove(List<Integer> solution) {
+        solution.remove(solution.size()-1);
+    }
+    
 
     public static void main(String[] args) {
-        System.out.println(ClimbingWays.getClimbingWays1(10));
+//        System.out.println(ClimbingWays.getClimbingWays1(10));
+        System.out.println(ClimbingWays.getClimbingWays4(5));
     }
 
 }
