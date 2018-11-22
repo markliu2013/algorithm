2c587169eb5cd9056ea8cf0306ab0571799f58f5
Mark Liu <markliu2013@gmail.com>
2018-11-22 20:00:50 +0800

    Pack01

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/pack/Pack01.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/pack/Pack01.java
@@ -1,18 +1,31 @@
 package com.zfwhub.algorithm.leetcode.pack;
 
 // 01背包
-// 
+// https://zhuanlan.zhihu.com/p/35278858
 public class Pack01 {
     
     public static int solution1(int[] volumns, int[] values, int capacity) {
-        
-        return 0;
+        int N = values.length;
+        int[][] F = new int[N+1][capacity+1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= capacity; j++) {
+                if (volumns[i-1] > j) {//如果物品放不进背包
+                    F[i][j] = F[i-1][j];
+                } else {
+                    F[i][j] = Math.max(F[i-1][j], F[i-1][j-volumns[i-1]]+values[i-1]);
+                }
+            }
+        }
+        return F[N][capacity];
     }
     
     public static void main(String[] args) {
         int[] volumns = new int[] {5,5,3,4,3};
         int[] values = new int[] {400,500,200,300,350};
         int capacity = 10;
+        /*int[] volumns = new int[] {2,3,4,5};
+        int[] values = new int[] {3,4,5,6};
+        int capacity = 8;*/
         System.out.println(solution1(volumns, values, capacity));
     }
 
