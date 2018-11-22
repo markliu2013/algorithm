21a3c6288421ef30e19b0d86556eb83246363448
Mark Liu <markliu2013@gmail.com>
2018-11-22 17:52:58 +0800

    Pack

---
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/pack/Pack01.java
@@ -0,0 +1,19 @@
+package com.zfwhub.algorithm.leetcode.pack;
+
+// 01背包
+// 
+public class Pack01 {
+    
+    public static int solution1(int[] volumns, int[] values, int capacity) {
+        
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        int[] volumns = new int[] {5,5,3,4,3};
+        int[] values = new int[] {400,500,200,300,350};
+        int capacity = 10;
+        System.out.println(solution1(volumns, values, capacity));
+    }
+
+}
