538f7fbf853375d790af61eefc471621a9298196
Mark Liu <markliu2013@gmail.com>
2018-11-23 21:54:22 +0800

    SubSets

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/Combinations.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/Combinations.java
@@ -6,28 +6,6 @@ import java.util.*;
 public class Combinations {
 
     /**
-     * assume we have arr [1,2,3], try to get all of combinations
-     * C(3,0)+C(3,1)+C(3,2)+C(3,3)=1+3+3+1=8=pow(2,3)
-     * try to look the array elements as turn off switch, so we can have pow(2,3)
-     */
-    public static List<List<Integer>> getAllCombination(int[] arr) {
-        List<List<Integer>> list = new ArrayList<List<Integer>>();
-        int count = new Double(Math.pow(2.0, arr.length)).intValue();
-        for (int i = 0; i < count; i++) {
-            List<Integer> list1 = new ArrayList<Integer>();
-            String binaryString = String.format("%3s", Integer.toBinaryString(i)).replace(' ', '0');
-            char[] chars = binaryString.toCharArray();
-            for (int j = 0; j < chars.length; j++) {
-                if (chars[j] == '1') {
-                    list1.add(arr[j]);
-                }
-            }
-            list.add(list1);
-        }
-        return list;
-    }
-
-    /**
      * get combinations of n elements from list, 
      * dynamic programming, pascal's triangle
      */
