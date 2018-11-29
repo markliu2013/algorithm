c83215751d253d610b8662fbc7ea91c20828ebe9
Mark Liu <markliu2013@gmail.com>
2018-11-29 20:10:23 +0800

    WordSearchII

---
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/WordSearchII.java
@@ -0,0 +1,33 @@
+package com.zfwhub.algorithm.leetcode.backtracking;
+
+import java.util.*;
+
+public class WordSearchII {
+    
+    // TODO WordSearchII Time Limit Exceeded
+    public static List<String> findWords(char[][] board, String[] words) {
+        List<String> list = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+        for (String word : words) {
+            set.add(word);
+        }
+        for (String word : set) {
+            if (WordSearch2.exist(board, word)) {
+                list.add(word);
+            }
+        }
+        return list;
+    }
+    
+    public static void main(String[] args) {
+        char[][] board1 = { 
+                { 'o', 'a' , 'a' , 'n' }, 
+                { 'e', 't' , 'a' , 'e' }, 
+                { 'i', 'h' , 'k' , 'r' },
+                { 'i', 'f' , 'l' , 'v' }
+        };
+        String[] words1 = new String[] {"oath","pea","eat","rain"};
+        System.out.println(findWords(board1, words1));
+    }
+
+}
