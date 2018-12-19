538d49847eb5fee4550c95b4b28aa2f9fd29b8b7
markliu2013 <markliu2013@gmail.com>
2018-12-19 13:01:16 +0800

    StickersToSpellWord Not Finish

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/pack/Pack04.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/pack/Pack04.java
@@ -1,9 +1,25 @@
 package com.zfwhub.algorithm.leetcode.pack;
-
+import java.util.*;
 /**
  * 混合三种背包问题
  * 例题：http://acm.hdu.edu.cn/showproblem.php?pid=3535
+ * http://www.cnblogs.com/llkpersonal/p/4055539.html
  */
 public class Pack04 {
+    
+    public static int solution1(List<Job> jobs, int capacity) {
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
 
+class Job {
+    //0 最少一个； 1 最多一个； 2 无限制
+    public int type;
+    public int cost;
+    public int value;
 }
