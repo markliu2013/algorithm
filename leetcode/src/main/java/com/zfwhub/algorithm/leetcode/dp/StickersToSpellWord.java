3de4b57b44287158a00cc2ff08380223b441564a
Mark Liu <markliu2013@gmail.com>
2018-12-27 21:46:05 +0800

    StickersToSpellWord Not Finish

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/StickersToSpellWord.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/StickersToSpellWord.java
@@ -29,21 +29,14 @@ public class StickersToSpellWord {
     }
     
     public static int dp(String[] stickers, List<Character> target) {
-        if (stickers.length == 1) {
-            Iterator<Character> targetIt = target.iterator();
-            String lastSticker = stickers[0];
-            List<Character> list2 = new ArrayList<>();
-            for (int i = 0; i < lastSticker.length(); i++) {
-                list2.add(lastSticker.charAt(i));
-            }
-            while (targetIt.hasNext()) {
-                Character c = targetIt.next();
-                if (list2.contains(c)) {
-                    targetIt.remove();
-                    list2.remove(c);
+        if (target.size() == 1) {
+            Set<Character> set2 = new HashSet<>();
+            for (int i = 0; i < stickers.length; i++) {
+                for (int j = 0; j < stickers[i].length(); j++) {
+                    set2.add(stickers[i].charAt(j));
                 }
             }
-            if (target.size() == 0) {
+            if (set2.contains(target.get(0))) {
                 return 1;
             } else {
                 return -1;
@@ -51,48 +44,23 @@ public class StickersToSpellWord {
         }
         String[] subStickers = Arrays.copyOf(stickers, stickers.length-1);
         String lastSticker = stickers[stickers.length-1];
-        List<Character> list2 = new ArrayList<>();
-        for (int i = 0; i < lastSticker.length(); i++) {
-            list2.add(lastSticker.charAt(i));
+        // lastSticker不包含target中任何一个
+        if (!contains(lastSticker, target)) {
+            return dp(subStickers, target);
+        } else {
+            
         }
-        boolean flag1 = false;
-        for (int i = 0; i < list1.size(); i++) {
-            if (list2.contains(list1.get(i))) {
-                flag1 = true;
-                break;
+        return 0;
+    }
+    
+    // sticker是否包含target中的某个字符
+    public static boolean contains(String sticker, List<Character> target) {
+        for (int i = 0; i < target.size(); i++) {
+            if (sticker.indexOf(target.get(i)) < 0) {
+                return true;
             }
         }
-        if (!flag1) {
-            return minStickers(subStickers, target);
-        }
-        boolean flag2 = true;
-        List<Character> list3 = new ArrayList<>();
-        int count = 0; // 记录最后一个用多少次
-        int minCount = Integer.MAX_VALUE;
-        while (flag2) {
-            list3.addAll(list2);
-            Iterator<Character> targetIt = list1.iterator();
-            boolean flag3 = true;
-            while (targetIt.hasNext()) {
-                Character c = targetIt.next();
-                if (list3.contains(c)) {
-                    targetIt.remove();
-                    list3.remove(c);
-                    flag3 = false;
-                    count++;
-                    StringBuilder sb = new StringBuilder();
-                    for (int i = 0; i < list1.size(); i++) {
-                        sb.append(list1.get(i));
-                    }
-                    int count1 = dp(subStickers, sb.toString());
-                    minCount = Math.min(minCount, count+count1);
-                }
-            }
-            if (flag3) {
-                flag2 = false; 
-            }
-        }
-        return minCount;
+        return false;
     }
     
     public static void main(String[] args) {
