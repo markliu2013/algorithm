114f3d7bd43305fea4c047595bb34484012b3db0
Mark Liu <markliu2013@gmail.com>
2018-12-29 12:12:18 +0800

    StickersToSpellWord Not Finished

--- leetcode/src/test/java/com/zfwhub/algorithm/leetcode/dp/StickersToSpellWordTest.java
+++ leetcode/src/test/java/com/zfwhub/algorithm/leetcode/dp/StickersToSpellWordTest.java
@@ -6,25 +6,37 @@ import org.junit.Test;
 
 public class StickersToSpellWordTest {
     
-    String[] stickers1 = new String[] {"with", "example", "science"};
-    String target1 = "thehat";
-    int expected1 = 3;
+    String[] stickers1 = new String[] {"a"};
+    String target1 = "a";
+    int expected1 = 1;
     
-    String[] stickers2 = new String[] {"notice", "possible"};
-    String target2 = "basicbasic";
-    int expected2 = -1;
+    String[] stickers2 = new String[] {"a", "b"};
+    String target2 = "ab";
+    int expected2 = 2;
     
-    String[] stickers3 = new String[] {"a"};
-    String target3 = "a";
+    String[] stickers3 = new String[] {"ab", "b"};
+    String target3 = "ab";
     int expected3 = 1;
     
-    String[] stickers4 = new String[] {"a", "b"};
+    String[] stickers4 = new String[] {"ab", "c"};
     String target4 = "ab";
-    int expected4 = 2;
+    int expected4 = 1;
     
-    String[] stickers5 = new String[] {"fly","me","charge","mind","bottom"};
-    String target5 = "centorder";
-    int expected5 = 4;
+    String[] stickers5 = new String[] {"aa", "baa", "ca"};
+    String target5 = "aa";
+    int expected5 = 1;
+    
+    String[] stickers21 = new String[] {"with", "example", "science"};
+    String target21 = "thehat";
+    int expected21 = 3;
+    
+    String[] stickers22 = new String[] {"notice", "possible"};
+    String target22 = "basicbasic";
+    int expected22 = -1;
+    
+    String[] stickers23 = new String[] {"fly","me","charge","mind","bottom"};
+    String target23 = "centorder";
+    int expected23 = 4;
     
     @Test
     public void testMinStickers() {
@@ -33,6 +45,9 @@ public class StickersToSpellWordTest {
         assertEquals(expected3, StickersToSpellWord.minStickers(stickers3, target3));
         assertEquals(expected4, StickersToSpellWord.minStickers(stickers4, target4));
         assertEquals(expected5, StickersToSpellWord.minStickers(stickers5, target5));
+        assertEquals(expected21, StickersToSpellWord.minStickers(stickers21, target21));
+        assertEquals(expected22, StickersToSpellWord.minStickers(stickers22, target22));
+        assertEquals(expected23, StickersToSpellWord.minStickers(stickers23, target23));
     }
 
 }
