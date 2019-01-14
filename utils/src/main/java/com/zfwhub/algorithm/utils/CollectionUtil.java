9f0d0f589fb06a1de6c87d3532dee562f8655afa
Mark Liu <markliu2013@gmail.com>
2019-01-14 22:30:18 +0800

    CollectionUtil subsetsWithDup Faster

--- utils/src/main/java/com/zfwhub/algorithm/utils/CollectionUtil.java
+++ utils/src/main/java/com/zfwhub/algorithm/utils/CollectionUtil.java
@@ -1,49 +1,63 @@
 package com.zfwhub.algorithm.utils;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 public class CollectionUtil {
     
     private CollectionUtil() { }
 
-    // TODO Convert a generic list to an array
-    // https://www.cnblogs.com/grandyang/p/4309345.html 解法一
     /**
-     * list的所有组合
+     * list的所有组合，list中不能有重复元素。请确认list中无重复元素，否则请使用subsetsWithDup
      * @param list
      * @return
      */
-    public static <T extends Comparable<? super T>> List<List<T>> subset(List<T> list) {
-        List<List<T>> result = new ArrayList<>();
-        Collections.sort(list);
-        result.add(new ArrayList<>());
-        for (int i = 0; i < list.size(); i++) {
-            int size = result.size();
-            for (int j = 0; j < size; j++) {
-                List<T> solution = new ArrayList<>(result.get(j));
-                solution.add(list.get(i));
-                result.add(solution);
-            }
+    public static <T> List<List<T>> subsets(List<T> list) {
+        List<List<T>> solutionList = new ArrayList<>();
+        subsetsHelper(solutionList, new ArrayList<>(), list, 0);
+        return solutionList;
+    }
+    
+    // backtrack
+    private static <T> void subsetsHelper(List<List<T>> solutionList, List<T> solution, List<T> list, int start) {
+        solutionList.add(new ArrayList<>(solution));
+        for (int i = start; i < list.size(); i++) {
+            solution.add(list.get(i));
+            subsetsHelper(solutionList, solution, list, i+1);
+            solution.remove(solution.size()-1);
         }
-        return result;
     }
     
-    public static <T extends Comparable<? super T>> List<List<T>> subset(T[] array) {
-        return subset(Arrays.asList(array));
+    public static <T> List<List<T>> subsets(T[] array) {
+        return subsets(Arrays.asList(array));
     }
     
-    public static <T extends Comparable<? super T>> Set<List<T>> subsetsWithDup(List<T> list) {
-        return new HashSet<>(subset(list));
+    /**
+     * list的所有组合，list中可以有重复元素。当年不确定是否list有重复。
+     * @param list
+     * @return
+     */
+    public static <T extends Comparable<? super T>> List<List<T>> subsetsWithDup(List<T> list) {
+        List<List<T>> solutionList = new ArrayList<>();
+        Collections.sort(list); //必须加上排序，以方便去重。
+        subsetsWithDupHelper(solutionList, new ArrayList<>(), list, 0);
+        return solutionList;
     }
     
-    public static <T extends Comparable<? super T>> Set<List<T>> subsetsWithDup(T[] array) {
-        return new HashSet<>(subset(array));
+    private static <T extends Comparable<? super T>> void subsetsWithDupHelper(List<List<T>> solutionList, List<T> solution, List<T> list, int start) {
+        solutionList.add(new ArrayList<>(solution));
+        for (int i = start; i < list.size(); i++) {
+            if(i > start && list.get(i) == list.get(i-1)) continue; // skip duplicates
+            solution.add(list.get(i));
+            subsetsWithDupHelper(solutionList, solution, list, i+1);
+            solution.remove(solution.size()-1);
+        }
+    }
+    
+    public static <T extends Comparable<? super T>> List<List<T>> subsetsWithDup(T[] array) {
+        return subsetsWithDup(Arrays.asList(array));
     }
     
     /**
