682eeb057e8e0365e4b372e5918bcaa7c22c2089
Mark Liu <markliu2013@gmail.com>
2019-01-14 22:27:22 +0800

    ArrayUtil toObject toPrimitive for Integer

--- utils/src/main/java/com/zfwhub/algorithm/utils/ArrayUtil.java
+++ utils/src/main/java/com/zfwhub/algorithm/utils/ArrayUtil.java
@@ -1,5 +1,34 @@
 package com.zfwhub.algorithm.utils;
 
 public class ArrayUtil {
+    
+    public static final int[] EMPTY_INT_ARRAY = new int[0];
+    public static final Integer[] EMPTY_INTEGER_OBJECT_ARRAY = new Integer[0];
+
+    public static Integer[] toObject(final int[] array) {
+        if (array == null) {
+            return null;
+        } else if (array.length == 0) {
+            return EMPTY_INTEGER_OBJECT_ARRAY;
+        }
+        final Integer[] result = new Integer[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = Integer.valueOf(array[i]);
+        }
+        return result;
+    }
+    
+    public static int[] toPrimitive(final Integer[] array) {
+        if (array == null) {
+            return null;
+        } else if (array.length == 0) {
+            return EMPTY_INT_ARRAY;
+        }
+        final int[] result = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i].intValue();
+        }
+        return result;
+    }
 
 }
