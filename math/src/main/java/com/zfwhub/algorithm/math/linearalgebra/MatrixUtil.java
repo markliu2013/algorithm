d5dc71f1dceaa566307cd3f1a9503a08fbfa0aea
Mark Liu <markliu2013@gmail.com>
2019-01-26 16:00:22 +0800

    save

---
+++ math/src/main/java/com/zfwhub/algorithm/math/linearalgebra/MatrixUtil.java
@@ -0,0 +1,26 @@
+package com.zfwhub.algorithm.math.linearalgebra;
+
+public class MatrixUtil {
+    
+    private MatrixUtil() { }
+
+    /**
+     * 矩阵乘法
+     * @param a
+     * @param b
+     * @return
+     */
+    public static Matrix multiply(Matrix a, Matrix b) {
+        return null;
+    }
+    
+    /**
+     * 求零空间
+     * @param a
+     * @return
+     */
+    public static Matrix getNullSpace(Matrix a) {
+        return null;
+    }
+    
+}
