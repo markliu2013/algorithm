b86ac79f57fb43594dcafa54c7249a01040d65f2
markliu2013 <markliu2013@gmail.com>
2019-01-27 11:52:01 +0800

    save

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/MinimumSwapsIncreasing.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/MinimumSwapsIncreasing.java
@@ -1,6 +1,7 @@
 package com.zfwhub.algorithm.leetcode.dp;
 import java.util.*;
 import com.zfwhub.algorithm.utils.ArrayUtil;
+import com.zfwhub.algorithm.utils.CollectionUtil;
 
 // https://leetcode.com/problems/minimum-swaps-to-make-sequences-increasing/
 public class MinimumSwapsIncreasing {
@@ -12,7 +13,6 @@ public class MinimumSwapsIncreasing {
         dfs(solution, bestSolution, A, B);
         return countSolution(bestSolution);
     }
-    
     // 动态规划
     public static int minSwap2(int[] A, int[] B) {
         DPResult dpResult = new DPResult();
@@ -53,9 +53,7 @@ public class MinimumSwapsIncreasing {
                 } else if (minSwap+1 < (double)A.length/2) {
                     dpResult.minSwap = minSwap+1;
                     dpResult.lastSwapped = 1;
-                    if (a1 > b2 && b1 > a2) {
-                        dpResult.lastSwapped = -1;
-                    }
+                    
                 } else {
                     dpResult.minSwap = minSwap+1;
                     dpResult.lastSwapped = -1;
@@ -68,6 +66,23 @@ public class MinimumSwapsIncreasing {
                 dpResult.lastSwapped = 1;
             }
         }
+    }
+    
+    // 
+    public static int minSwap3(int[] A, int[] B) {
+        List<Integer> list = CollectionUtil.newIntList(A.length);
+        for (int i = 0; i <= list.size()/2; i++) {
+            List<List<Integer>> list2 = CollectionUtil.subsets(list, i);
+            for (int j = 0; j < list2.size(); j++) {
+                int[] newA = A.clone();
+                int[] newB = B.clone();
+                swap2(newA, newB, list2.get(j));
+                if (ArrayUtil.isIncreasing(newA) && ArrayUtil.isIncreasing(newB)) {
+                    return list2.get(j).size();
+                }
+            }
+        }
+        return -1;//无解
     }
     
     private static class DPResult {
@@ -163,12 +178,20 @@ public class MinimumSwapsIncreasing {
         }
     }
     
+    private static void swap2(int[] A, int[] B, List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            int index = list.get(i)-1;
+            int temp = A[index];
+            A[index] = B[index];
+            B[index] = temp;
+        }
+    }
+    
     public static void main(String[] args) {
-        int[] A = new int[] {0,7,8,10,10,11,12,13,19,18};
-        int[] B = new int[] {4,4,5, 7,11,14,15,16,17,20};
+        int[] A = new int[] {2,1,6,7,8,13,15,11,18,13,20,24,17,28,22,23,36,37,39,34,43,38,48,41,46,48,49,50,56,55,59,60,62,64,66,75,69,70,71,74,87,78,95,97,81,99,85,101,90,91,93,95,107,109,101,111,106,114,115,117,118,115,121,122,123,124,125,126,134,131,133,136,142,149,151,152,145,156,158,150,162,159,161,165,169,170,169,174,172,176,177,181,183,192,186,188,189,196,198,200};
+        int[] B = new int[] {0,4,10,11,12,9,10,16,12,19,15,16,25,20,33,34,27,29,32,40,35,45,40,50,51,52,53,55,52,58,58,61,62,66,71,68,78,81,83,84,75,91,79,80,98,83,100,89,102,103,105,106,96,98,110,105,113,109,110,111,112,120,116,118,126,130,131,133,129,137,138,140,137,138,140,142,154,147,149,159,152,163,164,163,166,168,171,170,175,176,177,181,186,184,193,194,195,190,195,200};
         System.out.println(MinimumSwapsIncreasing.minSwap(A, B));
-//        System.out.println(MinimumSwapsIncreasing.minSwap2(A, B));
-//        System.out.println(1+1 < (double)5/2);
+//        System.out.println(MinimumSwapsIncreasing.minSwap3(A, B));
     }
 
 }
