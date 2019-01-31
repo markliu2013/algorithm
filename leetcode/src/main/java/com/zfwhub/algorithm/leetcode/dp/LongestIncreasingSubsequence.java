0c2515fadf9bbba497373691c9656224a429a305
Mark Liu <markliu2013@gmail.com>
2019-01-31 13:34:20 +0800

    save

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/LongestIncreasingSubsequence.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/LongestIncreasingSubsequence.java
@@ -1,10 +1,41 @@
 package com.zfwhub.algorithm.leetcode.dp;
 
+import java.util.Arrays;
+
 // https://leetcode.com/problems/longest-increasing-subsequence/
 public class LongestIncreasingSubsequence {
     
-    public int solution1(int[] nums) {
-        return 0;
+    public static int solution1(int[] nums) {
+        DPStatus dpStatus = new DPStatus();
+        solution1DP(nums, dpStatus);
+        return dpStatus.increasingCount;
+    }
+    
+    private static void solution1DP(int[] nums, DPStatus dpStatus) {
+        if (nums.length == 1) {
+            dpStatus.endNumber = nums[0];
+            dpStatus.increasingCount = 1;
+            return;
+        }
+        int lastNum = nums[nums.length-1];
+        int[] subNums = Arrays.copyOfRange(nums, 0, nums.length-1);
+        solution1DP(subNums, dpStatus);
+        int subEndNumber = dpStatus.endNumber;
+        int subIncreasingCount = dpStatus.increasingCount;
+        if (lastNum > subEndNumber) {
+            dpStatus.endNumber = lastNum;
+            dpStatus.increasingCount = subIncreasingCount+1;
+        }
+    }
+
+    static class DPStatus {
+        public int endNumber; //结尾的那个数
+        public int increasingCount; //
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = new int[] {1,2,1,3};
+        System.out.println(LongestIncreasingSubsequence.solution1(nums));
     }
     
 }
---
+++ leetcode/src/test/java/com/zfwhub/algorithm/leetcode/dp/LongestIncreasingSubsequenceTest.java
@@ -0,0 +1,19 @@
+package com.zfwhub.algorithm.leetcode.dp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LongestIncreasingSubsequenceTest {
+
+    @Test
+    public void testSolution() {
+        int[] nums1 = new int[] {1,2,1,3};
+        int expected1 = 2;
+        int[] nums2 = new int[] {10,9,2,5,3,7,101,18};
+        int expected2 = 4;
+        assertEquals(expected1, LongestIncreasingSubsequence.solution1(nums1));
+        assertEquals(expected2, LongestIncreasingSubsequence.solution1(nums2));
+    }
+
+}
