2cd844b01d943dc6ceb4bbfc8432ea7bdc77f770
Mark Liu <markliu2013@gmail.com>
2019-01-31 16:04:40 +0800

    save

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/LongestIncreasingSubsequence.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/LongestIncreasingSubsequence.java
@@ -8,13 +8,14 @@ public class LongestIncreasingSubsequence {
     public static int solution1(int[] nums) {
         DPStatus dpStatus = new DPStatus();
         solution1DP(nums, dpStatus);
-        return dpStatus.increasingCount;
+        return Math.max(dpStatus.increasingCount, dpStatus.increasingCountWithLast);
     }
     
     private static void solution1DP(int[] nums, DPStatus dpStatus) {
         if (nums.length == 1) {
             dpStatus.endNumber = nums[0];
             dpStatus.increasingCount = 1;
+            dpStatus.increasingCountWithLast = 1;
             return;
         }
         int lastNum = nums[nums.length-1];
@@ -22,19 +23,22 @@ public class LongestIncreasingSubsequence {
         solution1DP(subNums, dpStatus);
         int subEndNumber = dpStatus.endNumber;
         int subIncreasingCount = dpStatus.increasingCount;
+        int subIncreasingCountWithLast = dpStatus.increasingCountWithLast;
         if (lastNum > subEndNumber) {
             dpStatus.endNumber = lastNum;
             dpStatus.increasingCount = subIncreasingCount+1;
+            dpStatus.increasingCountWithLast = lastNum+1;
         }
     }
 
     static class DPStatus {
         public int endNumber; //结尾的那个数
         public int increasingCount; //
+        public int increasingCountWithLast;
     }
     
     public static void main(String[] args) {
-        int[] nums = new int[] {1,2,1,3};
+        int[] nums = new int[] {10,9,2,5,3,7};
         System.out.println(LongestIncreasingSubsequence.solution1(nums));
     }
     
--- leetcode/src/test/java/com/zfwhub/algorithm/leetcode/dp/LongestIncreasingSubsequenceTest.java
+++ leetcode/src/test/java/com/zfwhub/algorithm/leetcode/dp/LongestIncreasingSubsequenceTest.java
@@ -9,7 +9,7 @@ public class LongestIncreasingSubsequenceTest {
     @Test
     public void testSolution() {
         int[] nums1 = new int[] {1,2,1,3};
-        int expected1 = 2;
+        int expected1 = 3;
         int[] nums2 = new int[] {10,9,2,5,3,7,101,18};
         int expected2 = 4;
         assertEquals(expected1, LongestIncreasingSubsequence.solution1(nums1));
