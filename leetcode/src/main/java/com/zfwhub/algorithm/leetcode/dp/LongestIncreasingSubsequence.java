b50872c7572e596402da50ab682c99f6c2cecffa
Mark Liu <markliu2013@gmail.com>
2019-01-31 16:05:30 +0800

    save

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/LongestIncreasingSubsequence.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/dp/LongestIncreasingSubsequence.java
@@ -8,33 +8,30 @@ public class LongestIncreasingSubsequence {
     public static int solution1(int[] nums) {
         DPStatus dpStatus = new DPStatus();
         solution1DP(nums, dpStatus);
-        return Math.max(dpStatus.increasingCount, dpStatus.increasingCountWithLast);
+        return Math.max(dpStatus.increasingCountNotLast, dpStatus.increasingCountWithLast);
     }
     
     private static void solution1DP(int[] nums, DPStatus dpStatus) {
         if (nums.length == 1) {
-            dpStatus.endNumber = nums[0];
-            dpStatus.increasingCount = 1;
+            dpStatus.increasingCountNotLast = 0;
+            dpStatus.increasingCountNotLastEndNumber = null;
             dpStatus.increasingCountWithLast = 1;
             return;
         }
         int lastNum = nums[nums.length-1];
         int[] subNums = Arrays.copyOfRange(nums, 0, nums.length-1);
         solution1DP(subNums, dpStatus);
-        int subEndNumber = dpStatus.endNumber;
-        int subIncreasingCount = dpStatus.increasingCount;
+        int subIncreasingCount = dpStatus.increasingCountNotLast;
         int subIncreasingCountWithLast = dpStatus.increasingCountWithLast;
-        if (lastNum > subEndNumber) {
-            dpStatus.endNumber = lastNum;
-            dpStatus.increasingCount = subIncreasingCount+1;
-            dpStatus.increasingCountWithLast = lastNum+1;
+        if (lastNum > subNums[subNums.length-1]) {
         }
+        
     }
 
     static class DPStatus {
-        public int endNumber; //结尾的那个数
-        public int increasingCount; //
-        public int increasingCountWithLast;
+        public Integer increasingCountNotLast;
+        public Integer increasingCountNotLastEndNumber;
+        public Integer increasingCountWithLast;
     }
     
     public static void main(String[] args) {
