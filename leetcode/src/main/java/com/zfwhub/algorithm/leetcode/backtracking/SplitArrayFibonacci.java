fd8fe5494919f291e6fa95661471f20252bb0cd9
markliu2013 <markliu2013@gmail.com>
2019-02-03 09:47:47 +0800

    save

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/SplitArrayFibonacci.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/backtracking/SplitArrayFibonacci.java
@@ -6,6 +6,7 @@ import com.zfwhub.algorithm.utils.CollectionUtil;
 // https://leetcode.com/problems/split-array-into-fibonacci-sequence/
 public class SplitArrayFibonacci {
     
+    // Memory Limit Exceeded
     public static List<Integer> solution1(String S) {
         // 尝试分割string
         List<List<Integer>> list = CollectionUtil.subsets(CollectionUtil.newIntList(S.length()-1));
@@ -29,17 +30,21 @@ public class SplitArrayFibonacci {
                     String splitBitString = S.substring(preIndex);
                     if (splitBitString.startsWith("0") && splitBitString.length() > 1) {
                         splitValid = false;
-                        break;
                     } else {
                         splitResult.add(Integer.valueOf(splitBitString));
                     }
                 }
                 // 检查分割结果
-                boolean checkResult = true;
-                for (int j = 2; j < splitResult.size(); j++) {
-                    if (splitResult.get(j) != splitResult.get(j-1) + splitResult.get(j-2)) {
-                        checkResult = false;
-                        break;
+                if (splitValid) {
+                    boolean checkResult = true;
+                    for (int j = 2; j < splitResult.size(); j++) {
+                        if (splitResult.get(j) != splitResult.get(j-1) + splitResult.get(j-2)) {
+                            checkResult = false;
+                            break;
+                        }
+                    }
+                    if (checkResult && splitResult.size() > 2) {
+                        return splitResult;
                     }
                 }
             }
@@ -47,8 +52,20 @@ public class SplitArrayFibonacci {
         return new ArrayList<>();
     }
     
+    public static List<Integer> solution2(String S) {
+        List<Integer> result = new ArrayList<>();
+        List<Boolean> solution = new ArrayList<>();//每个位置是否打点
+        dfs(result, solution, S);
+        return result;
+    }
+
+    private static void dfs(List<Integer> result, List<Boolean> solution, String s) {
+        // TODO Auto-generated method stub
+        
+    }
+
     public static void main(String[] args) {
-        System.out.println(solution1("1234"));
+        System.out.println(solution2("214748364721474836422147483641"));
     }
     
 }
