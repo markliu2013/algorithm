fbd23573706ec0811153a1a188d879b223b7fae3
markliu2013 <markliu2013@gmail.com>
2019-02-03 19:06:38 +0800

    save

--- math/src/main/java/com/zfwhub/algorithm/math/linearalgebra/VectorUtil.java
+++ math/src/main/java/com/zfwhub/algorithm/math/linearalgebra/VectorUtil.java
@@ -11,5 +11,25 @@ public class VectorUtil {
     public static boolean isOrthogonal(Vector a, Vector b) {
         return false;
     }
+    
+    /**
+     * 点积
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int dotProducts(Vector a, Vector b) {
+        return 0;
+    }
+    
+    /**
+     * 叉积
+     * @param a
+     * @param b
+     * @return
+     */
+    public static Vector crossProducts(Vector a, Vector b) {
+        return null;
+    }
 
 }
