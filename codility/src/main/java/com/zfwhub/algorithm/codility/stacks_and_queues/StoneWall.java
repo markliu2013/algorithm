28a85283c2746d3e46a566d818d303c51a86143b
markliu2013 <markliu2013@gmail.com>
2019-02-11 21:31:30 +0800

    save

--- codility/src/main/java/com/zfwhub/algorithm/codility/stacks_and_queues/StoneWall.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/stacks_and_queues/StoneWall.java
@@ -1,13 +1,15 @@
 package com.zfwhub.algorithm.codility.stacks_and_queues;
 
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
 
 import com.zfwhub.algorithm.utils.CollectionUtil;
 
 // https://app.codility.com/programmers/lessons/7-stacks_and_queues/stone_wall/
 public class StoneWall {
 
-    // brute force, Performance bad
+    // brute force, Performance 44%
     public static int solution1(int[] H) {
         int count = 0;
         // set保存后面可以复用的stone
@@ -23,15 +25,37 @@ public class StoneWall {
         return count;
     }
 
-    // TODO StoneWall stack or quene
+    // Performance 88%
     public static int solution2(int[] H) {
-        return 0;
+        // list中永远都是从小到大
+        LinkedList<Integer> list = new LinkedList<>();
+        // 默认先把第一个放进list
+        list.add(H[0]);
+        int count = 1;
+        for (int i = 1; i < H.length; i++) {
+            if (H[i] <= list.get(list.size()-1)) { // 当前的比list最后一个小
+                int index = Collections.binarySearch(list, H[i]);
+                if (index < 0) { // list中没有
+                    index = Math.abs(index+1);
+                    count++;
+                }
+                // 清空在index之后的
+                list.subList(index, list.size()).clear();
+                list.add(H[i]);
+            } else {
+                count++;
+                list.add(H[i]);
+            }
+        }
+        return count;
     }
-
+    
+    // TODO StoneWall https://codility.com/media/train/solution-stone-wall.pdf
+    
     public static void main(String[] args) {
-        System.out.println(solution1(new int[] { 8, 8, 5, 7, 9, 8 }));
-        System.out.println(solution2(new int[] { 8, 8, 5, 7, 9, 8 }));
-        //        System.out.println(solution2(new int[] {1,2,3,1,2}));
-        //        System.out.println(solution3(new int[] {1,2,3,1,2}));
+//        int[] H = new int[] { 8, 8, 5, 7, 9, 8, 7,4,8 };
+        int[] H = new int[] { 1,2,1};
+        System.out.println(solution1(H));
+        System.out.println(solution2(H));
     }
 }
