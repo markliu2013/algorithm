468a155a76ceddef991d816a37e4f339c6c34f77
Mark Liu <markliu2013@gmail.com>
2019-02-14 17:48:17 +0800

    save

--- codility/src/main/java/com/zfwhub/algorithm/codility/dynamic_programming/MinAbsSum.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/dynamic_programming/MinAbsSum.java
@@ -16,12 +16,12 @@ public class MinAbsSum {
         }
         int result = Integer.MAX_VALUE;
         // 所有选择的组合，选的为1，不选的为-1。因为A中有重复元素，必须使用索引判断。
-        List<List<Integer>> list = CollectionUtil.subsets(CollectionUtil.newIntList(1, A.length));
+        List<List<Integer>> list = CollectionUtil.subsets(CollectionUtil.newIntList(0, A.length-1));
         for (List<Integer> list2 : list) {
             // 组合生成S数组
             int[] S = new int[A.length];
             for (int i = 0; i < A.length; i++) {
-                if (list2.contains(i+1)) {
+                if (list2.contains(i)) {
                     S[i] = 1;
                 } else {
                     S[i] = -1;
