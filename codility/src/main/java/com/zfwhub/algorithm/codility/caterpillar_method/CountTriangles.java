9a5709203909dffb14bce7954fd7974d2cefd9f2
Mark Liu <markliu2013@gmail.com>
2019-02-19 17:12:21 +0800

    save

--- codility/src/main/java/com/zfwhub/algorithm/codility/caterpillar_method/CountTriangles.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/caterpillar_method/CountTriangles.java
@@ -1,6 +1,11 @@
 package com.zfwhub.algorithm.codility.caterpillar_method;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.zfwhub.algorithm.utils.ArrayUtil;
+import com.zfwhub.algorithm.utils.CollectionUtil;
 
 // https://app.codility.com/programmers/lessons/15-caterpillar_method/count_triangles/
 public class CountTriangles {
@@ -20,8 +25,21 @@ public class CountTriangles {
         return count;
     }
 
-    // TODO CountTriangles https://www.cnblogs.com/grandyang/p/7053730.html
+    // collection util
     public static int solution2(int[] A) {
+        int count = 0;
+        List<List<Integer>> triples = CollectionUtil.combine(ArrayUtil.toList(A), 3);
+        for (List<Integer> triple : triples) {
+            Collections.sort(triple);
+            if (triple.get(0) + triple.get(1) > triple.get(2)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    // TODO CountTriangles https://www.cnblogs.com/grandyang/p/7053730.html
+    public static int solution3(int[] A) {
         Arrays.sort(A);
         
         return 0;
