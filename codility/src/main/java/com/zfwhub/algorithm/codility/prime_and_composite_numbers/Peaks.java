d7eb04800bded464b16fee40f6f3667771b4b7ae
Mark Liu <markliu2013@gmail.com>
2019-03-01 11:45:16 +0800

    save

--- codility/src/main/java/com/zfwhub/algorithm/codility/prime_and_composite_numbers/Peaks.java
+++ codility/src/main/java/com/zfwhub/algorithm/codility/prime_and_composite_numbers/Peaks.java
@@ -1,8 +1,7 @@
 package com.zfwhub.algorithm.codility.prime_and_composite_numbers;
 
 import java.util.*;
-
-import com.zfwhub.algorithm.utils.CollectionUtil;
+import com.zfwhub.algorithm.utils.*;
 
 // https://app.codility.com/programmers/lessons/10-prime_and_composite_numbers/peaks/
 public class Peaks {
@@ -33,27 +32,31 @@ public class Peaks {
                 }
             }
         }
+        // A中没有peak，返回0
+        return 0;
+    }
+    
+    // 效率比solution1块很多了
+    public static int solution2(int[] A) {
+        Set<Integer> peaks = getPeaks(A);
+        for (int i = peaks.size(); i > 0; i--) {
+            if (A.length % i == 0) {
+              // 将每个peak都尽量往右靠。如果没有重复的。
+              Set<Integer> set = new HashSet<>();
+              for (Integer peak : peaks) {
+                  set.add(NumberUtil.closestMultiple(peak, A.length/i, ClosestMultipleFlag.CEIL));
+              }
+              if (set.size() == peaks.size()) {
+                  return i;
+              }
+            }
+        }
         return 0;
     }
     
     // TODO Peaks 优化
-    public static int solution2(int[] A) {
-        Set<Integer> peaks = getPeaks(A);
-        int peakSize = peaks.size();
-        int result = 0;
-        if (peakSize == 0) {
-            return 1;
-        }
-        // 有几个peak则最多分成几块，将每个peak都尽量往右靠。如果没有重复的。
-//      Set<Integer> set = new HashSet<>();
-//      for (Integer peak : peaks) {
-//          set.add(NumberUtil.closestMultiple(peak, A.length/i, ClosestMultipleFlag.CEIL)-1);
-//      }
-//      System.out.println(set);
-//      if (set.size() == peaks.size()) {
-//          return i;
-//      }
-        return result;
+    public static int solution3(int[] A) {
+        return 0;
     }
     
     private static Set<Integer> getPeaks(int[] A) {
@@ -70,6 +73,7 @@ public class Peaks {
         int[] A = new int[] {1,2,3,4,3,4,1,2,3,4,6,2};
 //        int[] A = new int[] {0, 1, 0, 0, 1, 0, 0, 1, 0};
         System.out.println(solution1(A));
+        System.out.println(solution2(A));
     }
 
 }
