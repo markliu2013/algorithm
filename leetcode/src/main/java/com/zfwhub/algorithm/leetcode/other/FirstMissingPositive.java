b08d1337ca5fb5369719cf3a250a45a4b4e198e0
markliu2013 <markliu2013@gmail.com>
2019-03-20 17:52:08 +0800

    save

--- leetcode/src/main/java/com/zfwhub/algorithm/leetcode/other/FirstMissingPositive.java
+++ leetcode/src/main/java/com/zfwhub/algorithm/leetcode/other/FirstMissingPositive.java
@@ -45,6 +45,23 @@ public class FirstMissingPositive {
         //假设都符合要求，则返回长度+1的值
         return nums.length + 1;
     }
+    
+    public static int solution3(int[] A) {
+        int i = 0;
+        while(i < A.length){
+            if(A[i] == i+1 || A[i] <= 0 || A[i] > A.length) i++;
+            else if(A[A[i]-1] != A[i]) swap(A, i, A[i]-1);
+            else i++;
+        }
+        i = 0;
+        while(i < A.length && A[i] == i+1) i++;
+        return i+1;
+    }
+    private static void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
 
     public static void main(String[] args) {
         //        int[] nums = new int[] {-10,2,1,3,100,-1,-2,4,-3,6,5};
